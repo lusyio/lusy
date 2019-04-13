dad3baac8f84b2d6de56193f59441a75db05b6b8
Front <dimkaFPS@gmail.com>
2019-04-13 18:57:23 +0300

    Отредактирован task-control с переносом срока

--- engine/frontend/task/control/worker/postpone.php
+++ engine/frontend/task/control/worker/postpone.php
@@ -1,8 +1,7 @@
-<div class="text-center">
-<div id="status-block">
-	<p class="text-ligther"><?= $postponedate?></h1>
-	<p class="text-primary mb-4"><?=$GLOBALS["_newdate"]?></p>
+<div id="status-block" class="d-flex flex-row">
 	<button type="button" id="done" class="btn btn-primary mt-3 mb-3"><?=$GLOBALS["_completetask"]?></button>
+	<p class="text-ligther ml-3 mt-3"><?= $postponedate?></h1>
+	<p class="text-primary ml-3 mt-3"><?=$GLOBALS["_newdate"]?></p>
 </div>
 <div id="report-block" class="d-none">
 	<p class="text-ligther"><?=$GLOBALS["_writereport"]?>:</p>
@@ -11,5 +10,4 @@
 		<textarea name="report" id="reportarea" class="form-control" rows="4" placeholder="<?=$GLOBALS["_report"]?>" required></textarea>
 		<button type="submit" id="sendonreview" class="btn btn-primary w-100 text-center mt-3"><?=$GLOBALS["_sendpending"]?></button>
 	</div>
-</div>
 </div>
