ee7ec3d0cd579dc72e4e03b6a6e28869ee603574
Igor Garifyanov <garifianov.i@gmail.com>
2019-04-18 18:24:46 +0300

    addded response with request

--- engine/backend/task/task/control/manager/postpone.php
+++ engine/backend/task/task/control/manager/postpone.php
@@ -1,6 +1,6 @@
 <?php 
 
 $postponedate = date("d.m", strtotime(DBOnce('datepostpone','tasks','id='.$idtask)));
-
+$request = DBOnce('comment', 'comments', 'idtask=' . $idtask . ' order by `datetime` desc');
 
 ?>
