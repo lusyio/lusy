2f6d83a331d3f26708ebe26d32bfa2bb3a92e12f
Igor Garifyanov <garifianov.i@gmail.com>
2019-04-18 18:34:04 +0300

    added status condition

--- engine/backend/task/task/control/manager/postpone.php
+++ engine/backend/task/task/control/manager/postpone.php
@@ -1,6 +1,6 @@
 <?php 
 
 $postponedate = date("d.m", strtotime(DBOnce('datepostpone','tasks','id='.$idtask)));
-$request = DBOnce('comment', 'comments', 'idtask=' . $idtask . ' order by `datetime` desc');
+$request = DBOnce('comment', 'comments', "idtask=" . $idtask . " and status = 'request' order by `datetime` desc");
 
 ?>
