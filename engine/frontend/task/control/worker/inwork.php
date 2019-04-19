bd5d1b3ab683dd8f9c748dc19ac07499c01f6620
Front <dimkaFPS@gmail.com>
2019-04-19 17:14:17 +0300

    mindate

--- engine/frontend/task/control/worker/inwork.php
+++ engine/frontend/task/control/worker/inwork.php
@@ -20,7 +20,7 @@
     <div class="form-group">
         <div class="col-7">
             <textarea name="report" id="reportarea1" class="form-control" rows="4" placeholder="<?=$GLOBALS["_report"]?>" required></textarea>
-            <input class="form-control min-today" type="date" value="dateControl" id="example-date-input">
+            <input class="form-control" value="dateControl" type="date" id="example-date-input">
             <button type="button" id="backbutton1" class="btn btn-secondary w-30 text-center mt-3 mb-1" data-toggle="collapse" data-target="#change-date" aria-expanded="true" aria-controls="change-date"><?=$GLOBALS["_back"]?></button>
             <button type="submit" id="sendpostpone" class="btn btn-success text-center mt-3 mb-1"><?=$GLOBALS["_change"]?></button>
         </div>
