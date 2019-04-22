31ccd337c71cdef500f7c2cc49068994455d1aa3
Igor Garifyanov <garifianov.i@gmail.com>
2019-04-22 14:45:52 +0300

    fix to show actual comment in task body

--- engine/backend/task/task/control/worker/inwork.php
+++ engine/backend/task/task/control/worker/inwork.php
@@ -11,9 +11,10 @@ if ($dayost < 0) {
 	$text = $GLOBALS["_endfast"];
 	$head = $GLOBALS["_overdue"];
 }
+$request = DBOnce('comment, status', 'comments', "idtask=" . $idtask . " and (status = 'returned' or status = 'postpone') order by `datetime` desc");
+$messageStatus = DBOnce('status', 'comments', "idtask=" . $idtask . " and (status = 'returned' or status = 'postpone') order by `datetime` desc");
 $note = '';
-$request = DBOnce('comment', 'comments', "idtask=" . $idtask . " and status = 'returned' order by `datetime` desc");
-if ($request != '') {
+if ($request != false && $messageStatus == 'returned') {
 	$request = nl2br($request);
 	$note = 'Причина возврата:';
 }
