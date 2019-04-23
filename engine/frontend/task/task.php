e10464bae2178c97a66780dc232a5f1b0c65df2b
Igor Garifyanov <garifianov.i@gmail.com>
2019-04-23 13:43:03 +0300

    move statusbar to _frontend

--- engine/frontend/task/task.php
+++ engine/frontend/task/task.php
@@ -1,11 +1,96 @@
+<?php
+$statusBar = [
+    'new' => [
+        'border' => 'border-success',
+        'bg1' => 'bg-primary',
+        'bg2' => 'bg-custom-color',
+        'bg3' => 'bg-custom-color',
+        'ic1' => 'fas fa-bolt',
+        'ic2' => 'fas fa-eye',
+        'ic3' => 'fas fa-check',
+    ],
+    'inwork' => [
+        'border' => 'border-primary',
+        'bg1' => 'bg-primary',
+        'bg2' => 'bg-custom-color',
+        'bg3' => 'bg-custom-color',
+        'ic1' => 'fas fa-bolt',
+        'ic2' => 'fas fa-eye',
+        'ic3' => 'fas fa-check',
+    ],
+    'overdue' => [
+        'border' => 'border-danger',
+        'bg1' => 'bg-danger',
+        'bg2' => 'bg-custom-color',
+        'bg3' => 'bg-custom-color',
+        'ic1' => 'fab fa-gripfire',
+        'ic2' => 'fas fa-eye',
+        'ic3' => 'fas fa-check',
+    ],
+    'postpone' => [
+        'border' => '',
+        'bg1' => 'bg-warning',
+        'bg2' => 'bg-custom-color',
+        'bg3' => 'bg-custom-color',
+        'ic1' => 'far fa-clock',
+        'ic2' => 'fas fa-eye',
+        'ic3' => 'fas fa-check',
+    ],
+    'pending' => [
+        'border' => 'border-warning',
+        'bg1' => 'bg-custom-color',
+        'bg2' => 'bg-warning',
+        'bg3' => 'bg-custom-color',
+        'ic1' => 'fas fa-bolt',
+        'ic2' => 'fas fa-eye',
+        'ic3' => 'fas fa-check',
+    ],
+    'returned' => [
+        'border' => 'border-primary',
+        'bg1' => 'bg-custom-color',
+        'bg2' => 'bg-custom-color',
+        'bg3' => 'bg-custom-color',
+        'ic1' => 'fas fa-bolt',
+        'ic2' => 'fas fa-eye',
+        'ic3' => 'fas fa-check',
+    ],
+    'done' => [
+        'border' => 'border-success',
+        'bg1' => 'bg-custom-color',
+        'bg2' => 'bg-custom-color',
+        'bg3' => 'bg-success',
+        'ic1' => 'fas fa-bolt',
+        'ic2' => 'fas fa-eye',
+        'ic3' => 'fas fa-check',
+    ],
+    'canceled' => [
+        'border' => '',
+        'bg1' => 'bg-custom-color',
+        'bg2' => 'bg-custom-color',
+        'bg3' => 'bg-danger',
+        'ic1' => 'fas fa-bolt',
+        'ic2' => 'fas fa-eye',
+        'ic3' => 'fas fa-times',
+    ],
+];
 
+if ($dayost < 0) {
+    $statusBar['postpone']['border'] = 'border-danger';
+};
+if ($view == 0) {
+    $statusBar[$status]['border'] = 'border-secondary';
+};
+if ($id == $worker and $view == 0) {
+    $statusBar[$status]['border'] = 'border-primary';
+}
+?>
 
 <div class="container-fluid" id="task">
 		<div class="row justify-content-center">
               <div class="col-12 col-lg-10">
                     <div class="card mb-5">
                         <div class="card-body">
-                            <h2 class="<?=$border?>"><?=$nametask?></h2>
+                            <h2 class="<?=$statusBar[$status]['border']?>"><?=$nametask?></h2>
                             <p class="text-ligther"><?=$viewState?></p>
                             <h6 class="text-ligther"><i class="far fa-calendar-times custom"></i>  <?=$datedone?> <?=$datepostpone?> <?=$GLOBALS["_$status"]?> </h6>
                             <div class="mt-5 mb-5 text-justify"><?=$description?></div>
@@ -21,9 +106,10 @@
                             <div class="row">
                                 <div class="col- ml-3" title="<?=$GLOBALS["_$status"]?>">
                                     <p class="font-weight-bold text-ligther text-uppercase">Статус</p>
-                                    <span class="status-icon rounded-circle noty-m <?=$bg1?>"><i class="<?=$ic1?> custom"></i></span>
-                                    <span class="status-icon rounded-circle noty-m <?=$bg2?>"><i class="<?=$ic2?> custom"></i></span>
-                                    <span class="status-icon-last rounded-circle noty-m <?=$bg3?>"><i class="<?=$ic3?> custom"></i></span>
+
+                                    <span class="status-icon rounded-circle noty-m <?=$statusBar[$status]['bg1']?>"><i class="<?=$statusBar[$status]['ic1']?> custom"></i></span>
+                                    <span class="status-icon rounded-circle noty-m <?=$statusBar[$status]['bg2']?>"><i class="<?=$statusBar[$status]['ic2']?> custom"></i></span>
+                                    <span class="status-icon-last rounded-circle noty-m <?=$statusBar[$status]['bg3']?>"><i class="<?=$statusBar[$status]['ic3']?> custom"></i></span>
                                 </div>
 
                                 <div class="col- ml-3">
@@ -39,7 +125,6 @@
                         </div>
                         </div>
                     </div>
-
                     <nav>
                         <div class="nav nav-tabs" id="nav-tab" role="tablist">
                             <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Лента (1)</a>
