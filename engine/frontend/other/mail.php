199bbad2cd5cf2d37f014c885485b5252c1e3c4a
Igor Garifyanov <garifianov.i@gmail.com>
2019-04-25 16:26:00 +0300

    remove message form, fix typo

--- engine/frontend/other/mail.php
+++ engine/frontend/other/mail.php
@@ -1,30 +1,5 @@
 <div class="card">
 	<div class="card-header">
-		<h4 class="mb-0">Новое сообщение</h4>
-	</div>
-	<div class="card-body">
-		<form method="post">
-			<div class="form-group">
-			    <label for="mes">Сообщение</label>
-			    <textarea class="form-control" id="mes" name="mes" rows="3" placeholder="Текст сообщения" required></textarea>
-			  </div>
-			  <div class="form-group">
-			    <label for="recipient">Получатель</label>
-			    <select class="form-control" id="recipient" name="recipient">
-					<?php
-					$users = DB('*','users','idcompany='.$GLOBALS["idc"].' and id !='.$GLOBALS["id"]);
-					foreach ($users as $n) { ?>
-					    <option value="<?php echo $n['id'] ?>"><?php echo $n['login'] ?></option>
-					<?php } ?>
-			    </select>
-			  </div>
-			  <button type="submit" class="btn btn-primary">Отправить</button>
-		</form>
-	</div>
-</div>
-
-<div class="card mt-3">
-	<div class="card-header">
 		<h4 class="mb-0">Диалоги</h4>
 	</div>
 	<div class="list-group">
@@ -40,7 +15,7 @@
 </div>
 <div class="card mt-3">
     <div class="card-header">
-        <h4 class="mb-0">Позьзователи</h4>
+        <h4 class="mb-0">Пользователи</h4>
     </div>
     <div class="list-group">
         <?php foreach ($userList as $user): ?>
