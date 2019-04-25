24ff4d1ca303ed02615fc992da58c536be003fe8
Igor Garifyanov <garifianov.i@gmail.com>
2019-04-25 12:25:10 +0300

    addlinks on attached files to task report sticky note

--- engine/backend/task/task/control/worker/pending.php
+++ engine/backend/task/task/control/worker/pending.php
@@ -1,5 +1,9 @@
 <?php 
 $pendingdate = date("d.m.Y");
 $report = DBOnce('comment', 'comments', "idtask=" . $idtask . " and status = 'report' order by `datetime` desc");
+$commentId = DBOnce('id', 'comments', "idtask=" . $idtask . " and status = 'report' order by `datetime` desc");
 $report = nl2br($report);
+$filesQuery = $pdo->prepare('SELECT file_id, file_name, file_size, file_path, comment_id FROM uploads WHERE comment_id = :commentId and comment_type = :commentType');
+$filesQuery->execute(array(':commentId' => $commentId, ':commentType' => 'comment'));
+$files = $filesQuery->fetchAll(PDO::FETCH_ASSOC);
 ?>
