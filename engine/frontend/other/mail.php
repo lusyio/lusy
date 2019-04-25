995c30e69fe7f5c81070230fd6753ac09e57e554
Igor Garifyanov <garifianov.i@gmail.com>
2019-04-25 18:21:06 +0300

    add user dropdown in header of card

--- engine/frontend/other/mail.php
+++ engine/frontend/other/mail.php
@@ -1,30 +1,30 @@
 <div class="card">
-	<div class="card-header">
-		<h4 class="mb-0">Диалоги</h4>
-	</div>
-	<div class="list-group">
-	<?php foreach ($dialog as $n) { ?>
-	
-	<a href="./<?= $n ?>/" class="list-group-item list-group-item-action border-0">
-		<p class="font-weight-bold"><?=fiomess($n)?></p>
-		<p><?=lastmess($n)?></p>
-	</a>
-	
-	<?php } ?>
-	</div>
-</div>
-<div class="card mt-3">
     <div class="card-header">
-        <h4 class="mb-0">Пользователи</h4>
+        <div class="row mb-3">
+            <div class="col-10"><h4 class="mb-0">Диалоги</h4></div>
+            <div class="col-2 text-right">
+                <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseUsers"
+                        aria-expanded="false" aria-controls="collapseExample">+
+                </button>
+            </div>
+        </div>
+        <div class="collapse list-group" id="collapseUsers">
+            <?php foreach ($userList as $user): ?>
+                <a href="./<?= $user['id'] ?>/" class="list-group-item list-group-item-action border-0">
+                    <p class="font-weight-bold"><?= $user['name'] . ' ' . $user['surname'] ?></p>
+                </a>
+            <?php endforeach; ?>
+        </div>
     </div>
     <div class="list-group">
-        <?php foreach ($userList as $user): ?>
-            <a href="./<?= $user['id'] ?>/" class="list-group-item list-group-item-action border-0">
-                <p class="font-weight-bold"><?= $user['name'] . ' ' . $user['surname'] ?></p>
+        <?php foreach ($dialog as $n) { ?>
+            <a href="./<?= $n ?>/" class="list-group-item list-group-item-action border-0">
+                <p class="font-weight-bold"><?= fiomess($n) ?></p>
+                <p><?= lastmess($n) ?></p>
             </a>
-        <?php endforeach; ?>
+        <?php } ?>
     </div>
-	
 </div>
+
 
 
