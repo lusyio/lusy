6525255df1202ecc2f2d832287e5758394beb823
Igor Garifyanov <garifianov.i@gmail.com>
2019-04-29 11:15:10 +0300

    comet settings

--- index.php
+++ index.php
@@ -214,5 +214,14 @@ if (!isMailTableExists()) {
     $sql->execute();
 }
 
+//connection to comet-server
+
+$cometUser = '2553';
+$cometPass = 'ywg03ajXvYGrtoCp6pvy7GEFf4hFGW5IeXQMBtF8i53S7ZgfXHVyWmIVwqPGel8r';
+$cometDsn = 'mysql:host=app.comet-server.ru;dbname=CometQL_v1';
+$cometPdo = new PDO($cometDsn, $cometUser, $cometPass);
+$cometPdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
 include 'engine/backend/other/footer.php';
 include 'engine/frontend/other/footer.php';
