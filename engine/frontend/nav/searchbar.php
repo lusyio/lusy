11b038d3bd2c8326c6984e16913d63668626359c
Front <dimkaFPS@gmail.com>
2019-04-29 17:14:12 +0300

    Update searchbar.php

--- engine/frontend/nav/searchbar.php
+++ engine/frontend/nav/searchbar.php
@@ -13,6 +13,11 @@
             </div>
 
             <div class="d-inline-block mb-2 ml-5">
+                <?php foreach ($usedStatuses as $status): ?>
+                    <div id="<?= $status[0] ?>Search" rel="<?= $status[0] ?>" class="btn btn-secondary words-search status-search">
+                        <span><?=$GLOBALS["_{$status[0]}filter"]?></span>
+                    </div>
+                <?php endforeach; ?>
                 <?php if($isManager): ?>
                 <div id="managerSearch" rol="manager" class="btn btn-secondary words-search role-search">
                     <span><?=$GLOBALS["_managerfilter"]?></span>
@@ -23,11 +28,6 @@
                     <span><?=$GLOBALS["_workerfilter"]?></span>
                 </div>
                 <?php endif; ?>
-                <?php foreach ($usedStatuses as $status): ?>
-                    <div id="<?= $status[0] ?>Search" rel="<?= $status[0] ?>" class="btn btn-secondary words-search status-search">
-                        <span><?=$GLOBALS["_{$status[0]}filter"]?></span>
-                    </div>
-                <?php endforeach; ?>
             </div>
 
         </div>
