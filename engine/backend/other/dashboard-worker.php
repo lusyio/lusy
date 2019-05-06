f16e178f4b6ad696dadd6a9832afff81420b0a1d
Kelevra <kelevra@MacBook-Pro-Dmitrij-2.local>
2019-05-06 14:43:43 +0300

    Mega pravki DashBoard Super-Puper

--- engine/backend/other/dashboard-worker.php
+++ engine/backend/other/dashboard-worker.php
@@ -1,1 +1,12 @@
 <?php
+global $id;
+global $_overdue;
+global $_pending;
+global $_inprogress;
+
+$worker = DBOnce('COUNT(*)','tasks','worker='.$id);
+$manager = DBOnce('COUNT(*)','tasks','manager='.$id);
+$all = $worker + $manager;
+$inwork = DBOnce('COUNT(*) as count','tasks','(status="new" or status="inwork" or status="returned") and worker='.$id);
+$pending = DBOnce('COUNT(*) as count','tasks','worker='.$id.' and status="pending"');
+$overdue = DBOnce('COUNT(*) as count','tasks','worker='.$id.' and status="overdue"');
