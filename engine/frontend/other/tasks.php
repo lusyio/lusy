eee1b41c451f3076c4759ebd8e3bc958680f25a3
Igor Garifyanov <garifianov.i@gmail.com>
2019-05-06 18:18:46 +0300

    Update tasks.php

--- engine/frontend/other/tasks.php
+++ engine/frontend/other/tasks.php
@@ -14,54 +14,12 @@ $borderColor = [
     'done' => 'border-success',
     'canceled' => 'border-secondary',
 ];
-
-	$i = 0;
-    global $pdo;
-    global $_months;
-    $countAttachedFilesQuery = "SELECT COUNT(*) as count FROM `uploads` u LEFT JOIN comments c on u.comment_id=c.id AND u.comment_type='comment' WHERE (u.comment_type='task' AND u.comment_id=:idtask) OR (u.comment_type='comment' AND c.idtask=:idtask)";
-    $dbh = $pdo->prepare($countAttachedFilesQuery);
-	foreach ($tasks as $n):
-        $idtask = $n["id"];
-        $idworker = $n["worker"];
-        $idmanager = $n["manager"];
-        $status = $n["status"];
-        $name = $n["name"];
-		$countcomments = DBOnce('COUNT(*) as count','comments','status="comment" and idtask='.$idtask);
-        $dbh->execute(array(':idtask' => $idtask));
-        $countAttachedFiles = $dbh->fetchColumn(0);
-        $datecreate = $n['datecreate'];
-        $datedone = $n["datedone"];
-        if (!is_null($n['datepostpone']) && $n['datepostpone'] != '0000-00-00') {
-            $datedone = $n["datepostpone"];
-        }
-        $dateCreateDateDoneDiff = strtotime($datedone) - strtotime($datecreate);
-        if (strtotime($datedone) > time() && $dateCreateDateDoneDiff != 0) {
-            $daysTotal = $dateCreateDateDoneDiff / (60 * 60 * 24) + 1;
-            $daysPassed = ceil((time() - strtotime($datecreate)) / (60 * 60 * 24));
-            $dateProgress = round(($daysPassed) * 100 / $daysTotal);
-        } else {
-            $dateProgress = 100;
-        }
-        $deadLineDay = date('j', strtotime($datedone));
-        $deadLineMonth = $_months[date('n', strtotime($datedone)) - 1];
-        $role = '';
-        if ($id == $idworker) {
-            $role .= ' worker';
-        }
-        if ($id == $idmanager) {
-            $role .= ' manager';
-        }
-        $i++;
-        ?>
-
-
+	foreach ($tasks as $n): ?>
         <div class="task-card zoom">
-            <div class="card mb-2 tasks <?= $status ?><?= $role ?>">
-                <div class="card-body tasks-list">
-                    <div class="d-block border-left-tasks <?= $borderColor[$status] ?>">
-                        <a class="d-block position-absolute w-100 h-100" href="/task/<?= $idtask ?>/">
-                        <a class="h5 card-title mb-2"><span><?= $name ?></span></a>
-
+            <div class="card mb-2 tasks <?= $n['status'] ?><?= $n['role'] ?>">
+                <div class="card-body tasks-list" id="middleMouse" onclick="window.location='/task/<?= $n['idtask'] ?>/';">
+                    <div class="d-block border-left-tasks <?= $borderColor[$n['status']] ?>">
+                        <a><h5 class="card-title mb-2"><span><?= $n['name'] ?></span></h5></a>
                         <p class="font-weight-light">Новая задача. Ознакомьтесь.</p>
                         <div class="row">
 	                        <div class="col-sm-8">
