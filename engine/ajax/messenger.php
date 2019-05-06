ea5c8a25ecb3be37171e326eef2278d07c989a8c
Igor Garifyanov <garifianov.i@gmail.com>
2019-05-06 14:55:20 +0300

    move uploadAtachedFiles methods to separate file

--- engine/ajax/messenger.php
+++ engine/ajax/messenger.php
@@ -45,46 +45,6 @@ function fiomess($iduser)
     return $fio;
 }
 
-/**
- * Загружает каждый файл из массива _FILES в upload/files/
- * и добавляет информацию о нем в бд в таблицу uploads
- * @param string $type Type to which the files is attached: 'task' or 'comment'
- * @param int $id Id of specified type event
- */
-function uploadAttachedFiles($type, $id)
-{
-    global $pdo;
-    global $idc;
-    global $iduser;
-    $types = ['task', 'comment', 'conversation'];
-    if (!in_array($type, $types)) {
-        return;
-    }
-
-    if ($type == 'comment') {
-        global $idtask;
-    } else {
-        $idtask = $id;
-    }
-
-    $dirName = 'upload/files/' . $idtask;
-    if (!realpath($dirName)) {
-        mkdir($dirName, 0777, true);
-    }
-
-    $sql = $pdo->prepare('INSERT INTO `uploads` (file_name, file_size, file_path, comment_id, comment_type, company_id, is_deleted, author) VALUES (:fileName, :fileSize, :filePath, :commentId, :commentType, :companyId, :isDeleted, :author)');
-    foreach ($_FILES as $file) {
-        $fileName = basename($file['name']);
-        $hashName = md5_file($file['tmp_name']);
-        while (file_exists($dirName . '/' . $hashName)) {
-            $hashName = md5($hashName);
-        }
-        $filePath = $dirName . '/' . $hashName;
-        $sql->execute(array(':fileName' => $fileName, ':fileSize' => $file['size'], ':filePath' => $filePath, ':commentId' => $id, ':commentType' => $type, ':companyId' => $idc, ':isDeleted' => 0, ':author' => $iduser));
-        move_uploaded_file($file['tmp_name'], $filePath);
-    }
-}
-
 function getMessageById($messageId)
 {
     global $pdo;
---
+++ engine/backend/functions/common-functions.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Загружает каждый файл из массива _FILES в upload/files/
+ * и добавляет информацию о нем в бд в таблицу uploads
+ * @param string $type Type to which the files is attached: 'task' or 'comment'
+ * @param int $eventId Id of specified type event
+ */
+function uploadAttachedFiles($type, $eventId)
+{
+    global $pdo;
+    global $idc;
+    global $id;
+    $types = ['task', 'comment', 'conversation'];
+    if (!in_array($type, $types)) {
+        return;
+    }
+
+    if ($type == 'comment') {
+        global $idtask;
+    } else {
+        $idtask = $eventId;
+    }
+
+    $dirName = 'upload/files/' . $idtask;
+    if (!realpath($dirName)) {
+        mkdir($dirName, 0777, true);
+    }
+
+    $sql = $pdo->prepare('INSERT INTO `uploads` (file_name, file_size, file_path, comment_id, comment_type, company_id, is_deleted, author) VALUES (:fileName, :fileSize, :filePath, :commentId, :commentType, :companyId, :isDeleted, :author)');
+    foreach ($_FILES as $file) {
+        $fileName = basename($file['name']);
+        $hashName = md5_file($file['tmp_name']);
+        while (file_exists($dirName . '/' . $hashName)) {
+            $hashName = md5($hashName);
+        }
+        $filePath = $dirName . '/' . $hashName;
+        $sql->execute(array(':fileName' => $fileName, ':fileSize' => $file['size'], ':filePath' => $filePath, ':commentId' => $eventId, ':commentType' => $type, ':companyId' => $idc, ':isDeleted' => 0, ':author' => $id));
+        move_uploaded_file($file['tmp_name'], $filePath);
+    }
+}
