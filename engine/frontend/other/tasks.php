6fbd800658114b235152fb474a125edc3c93757b
Igor Garifyanov <garifianov.i@gmail.com>
2019-05-06 12:08:00 +0300

    change deadline format

--- engine/frontend/other/tasks.php
+++ engine/frontend/other/tasks.php
@@ -16,6 +16,7 @@ $borderColor = [
 ];
 	$i = 0;
     global $pdo;
+    global $_months;
     $countAttachedFilesQuery = "SELECT COUNT(*) as count FROM `uploads` u LEFT JOIN comments c on u.comment_id=c.id AND u.comment_type='comment' WHERE (u.comment_type='task' AND u.comment_id=:idtask) OR (u.comment_type='comment' AND c.idtask=:idtask)";
     $dbh = $pdo->prepare($countAttachedFilesQuery);
 	foreach ($tasks as $n):
@@ -27,6 +28,10 @@ $borderColor = [
 		$countcomments = DBOnce('COUNT(*) as count','comments','status="comment" and idtask='.$idtask);
         $dbh->execute(array(':idtask' => $idtask));
         $countAttachedFiles = $dbh->fetchColumn(0);
+        $datedone = $n["datedone"];
+        $deadLineDay = date('d', strtotime($n['datedone']));
+        $deadLineMonth = $_months[date('n', strtotime($n['datedone']))];
+
         $datedone = $n["datedone"];
         $role = '';
         if ($id == $idworker) {
@@ -46,7 +51,7 @@ $borderColor = [
                         <p class="font-weight-light">Новая задача. Ознакомьтесь.</p>
                         <div class="row">
 	                        <div class="col-sm-8">
-		                        <div class="informer d-inline p-2 rounded mr-1"><i class="far fa-calendar-times text-ligther"></i><span class="text-ligther ml-2">Дедлайн: </span><span><?= $datedone ?></span></div>
+		                        <div class="informer d-inline p-2 rounded mr-1"><i class="far fa-calendar-times text-ligther"></i><span class="text-ligther ml-2">Дедлайн: </span><span><?= $deadLineDay ?> <?= $deadLineMonth ?></span></div>
 		                        <div class="informer d-inline p-2 rounded mr-1"><i class="fas fa-comments"></i><span class="ml-2"><?=$countcomments?></span></div>
 		                        <div class="informer d-inline p-2 rounded"><i class="fas fa-file"></i><span class="ml-2"><?=$countAttachedFiles?></span></div>
 	                        </div>
