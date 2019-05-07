22409f6d7250413de56e8486f211a6b25ea948b9
Kelevra <kelevra@MacBook-Pro-Dmitrij-2.local>
2019-05-07 17:05:34 +0300

    main-nav uploads

--- engine/backend/nav/main-nav.php
+++ engine/backend/nav/main-nav.php
@@ -14,9 +14,11 @@ global $pdo;
 
 
 // количество задач
-
 $worker = DBOnce('COUNT(*)','tasks','worker='.$id);
 $manager = DBOnce('COUNT(*)','tasks','manager='.$id);
+
+// файловый прогресс бар
 $uploadlimit = 100;
 $totalSize = number_format((DBOnce('SUM(file_size) AS totalSize','uploads','company_id='.$idc)) / (1024 * 1024),0,'',' '); 
 $pros = ($totalSize/$uploadlimit)*100;
+if($pros > 90) { $bgpross = 'bg-danger'; } else { $bgpross = '';}
