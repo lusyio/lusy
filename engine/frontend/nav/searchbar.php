6f1605716309c4f75bfad2a94c32a73290e13e39
Kelevra <kelevra@MacBook-Pro-Dmitrij-2.local>
2019-05-13 12:06:18 +0300

    Test White design

--- engine/frontend/nav/searchbar.php
+++ engine/frontend/nav/searchbar.php
@@ -1,30 +1,34 @@
 <h3 class="pb-3"><?=$GLOBALS["_tasks"]?></h3>
-<input id="searchInput" autocomplete="off" class="form-control form-control-sm form-control-borderless mb-2" type="text" placeholder="<?=$GLOBALS["_searchplaceholder"]?>...">
 
-<div class="row">
-    <div class="col-sm-12 mb-2">
-
-            <div class="d-inline-block mb-2">
-                <?php if($isManager): ?>
-                <div id="managerSearch" rol="manager" class="btn btn-secondary words-search role-search">
-                    <span><?=$GLOBALS["_managerfilter"]?></span>
-                    <span class="count"></span>
-                </div>
-                <?php endif; ?>
-                <?php if($isWorker): ?>
-                <div id="workerSearch" rol="worker" class="btn btn-secondary words-search role-search">
-                    <span><?=$GLOBALS["_workerfilter"]?></span>
-                    <span class="count"></span>
-                </div>
-                <?php endif; ?>
-                <?php foreach ($usedStatuses as $status): ?>
-                    <div id="<?= $status[0] ?>Search" rel="<?= $status[0] ?>" class="btn btn-secondary words-search status-search">
-                        <span><?=$GLOBALS["_{$status[0]}filter"]?></span>
-                        <span class="count"></span>
-                    </div>
-                <?php endforeach; ?>
-
-                <div id="resetSearch" class="btn btn-secondary words-search-reset">Сброс<i class="fas fa-times ml-1"></i></div>
-            </div>
-    </div>
+<div class="card mb-3">
+	<div class="card-body pb-2">
+		<input id="searchInput" autocomplete="off" class="form-control form-control-sm form-control-borderless mb-2" type="text" placeholder="<?=$GLOBALS["_searchplaceholder"]?>...">
+		<div class="row">
+		    <div class="col-sm-12">
+		
+		            <div class="d-inline-block">
+		                <?php if($isManager): ?>
+		                <div id="managerSearch" rol="manager" class="btn btn-secondary words-search role-search">
+		                    <span><?=$GLOBALS["_managerfilter"]?></span>
+		                    <span class="count"></span>
+		                </div>
+		                <?php endif; ?>
+		                <?php if($isWorker): ?>
+		                <div id="workerSearch" rol="worker" class="btn btn-secondary words-search role-search">
+		                    <span><?=$GLOBALS["_workerfilter"]?></span>
+		                    <span class="count"></span>
+		                </div>
+		                <?php endif; ?>
+		                <?php foreach ($usedStatuses as $status): ?>
+		                    <div id="<?= $status[0] ?>Search" rel="<?= $status[0] ?>" class="btn btn-secondary words-search status-search">
+		                        <span><?=$GLOBALS["_{$status[0]}filter"]?></span>
+		                        <span class="count"></span>
+		                    </div>
+		                <?php endforeach; ?>
+		
+		                <div id="resetSearch" class="btn btn-secondary words-search-reset">Сброс<i class="fas fa-times ml-1"></i></div>
+		            </div>
+		    </div>
+		</div>
+	</div>
 </div>
--- engine/frontend/nav/top-sidebar.php
+++ engine/frontend/nav/top-sidebar.php
@@ -2,17 +2,17 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-3">
-				<a class="navbar-brand text-uppercase font-weight-bold visible-lg text-white" href="/"><?=$namec?></a>
+				<a class="navbar-brand text-uppercase font-weight-bold visible-lg" href="/"><?=$namec?></a>
 		        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"><i class="fas fa-bars"></i></button>
 			</div>
 			<div class="col-sm-4">
 				<input class="form-control mr-sm-2" id="search" type="text" autocomplete="off" placeholder="Search..." style=" margin-left: 8px; ">
 			</div>
 			<div class="col-sm-5">
-				<div class="float-right text-right">
-					<a href="/mail/" class="mr-3"><i class="fas fa-envelope <?= ($newMailCount) ? 'text-warning' : 'text-white' ?>" id="messagesIcon"></i> <strong class="text-warning" id="messagesCount"><?= ($newMailCount) ? $newMailCount : '' ?></strong></a>
-					<a href="/log/" class="mr-3"><i class="fas fa-bell text-white"></i></a>
-					<a href="/logout/" class="mr-3"><i class="fas fa-sign-out-alt text-white"></i></a>	
+				<div class="float-right text-right alerts">
+					<a href="/mail/" class="mr-3"><i class="fas fa-envelope <?= ($newMailCount) ? 'text-warning' : '' ?>" id="messagesIcon"></i> <strong class="text-warning" id="messagesCount"><?= ($newMailCount) ? $newMailCount : '' ?></strong></a>
+					<a href="/log/" class="mr-3"><i class="fas fa-bell"></i></a>
+					<a href="/logout/" class="mr-3"><i class="fas fa-sign-out-alt"></i></a>	
 					<a href="/profile/<?=$id?>/"><img class="user-img rounded-circle" src="/upload/avatar/<?=$id?>.jpg"/></a>
 				</div>
 			</div>
