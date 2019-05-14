4aa2c66f4ebb37308881fe27ce877cbb1252e792
Front <dimkaFPS@gmail.com>
2019-05-14 13:07:01 +0300

    update

--- engine/frontend/other/message.php
+++ engine/frontend/other/message.php
@@ -1,15 +1,13 @@
-<div class="message <?= ($message['owner']) ? 'my-message' : 'not-my-message'; ?> <?= ($message['view_status'] || $message['sender'] == $id) ? '' : 'alert-primary' ?>">
-    <p><?= $message['author'] ?> (<?= $message['datetime'] ?>)<?=$message['status']?>:</p>
-<p><?= nl2br($message['mes']) ?></p>
+<div class="mb-3 message <?= ($message['owner']) ? 'my-message' : 'not-my-message'; ?> <?= ($message['view_status'] || $message['sender'] == $id) ? '' : 'alert-primary' ?>">
+    <p class="m-0"><?= $message['author'] ?> <?=$message['status']?>:</p>
+<p class="m-0"><?= nl2br($message['mes']) ?></p>
 <?php if (count($message['files']) > 0): ?>
-    <p>Прикрепленные файлы</p>
     <?php foreach ($message['files'] as $file): ?>
         <?php if ($file['is_deleted']): ?>
-            <p class=""><s><?= $file['file_name'] ?></s> (удален)</p>
+            <p class="m-0"><s><?= $file['file_name'] ?></s> (удален)</p>
         <?php else: ?>
-            <p><a class="" href="../../<?= $file['file_path'] ?>"><?= $file['file_name'] ?></a></p>
+            <p class="m-0"><a class="" href="../../<?= $file['file_path'] ?>"><?= $file['file_name'] ?></a></p>
         <?php endif; ?>
     <?php endforeach; ?>
 <?php endif; ?>
 </div>
-<hr>
