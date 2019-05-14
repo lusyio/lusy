61fa558e08d90bfe312253467645f64b0c5fa297
Igor Garifyanov <garifianov.i@gmail.com>
2019-05-14 14:46:30 +0300

    fix last commit

--- engine/backend/other/header.php
+++ engine/backend/other/header.php
@@ -5,7 +5,12 @@ require_once 'engine/backend/functions/common-functions.php';
 	$url = str_replace('/', '', $url);
 	if (empty($_SESSION['auth']) && !empty($_COOKIE['token'])) {
         $sessionCookie = parseCookie($_COOKIE['token']);
-        if ($sessionCookie || !isCookieExistAndValidByTimestamp($sessionCookie)) {
+        if (!$sessionCookie) {
+            setcookie('token', null, -1, '/');
+            header('location: /login/');
+            ob_end_flush();
+        }
+        if (!isCookieExistAndValidByTimestamp($sessionCookie)) {
             removeSessions($sessionCookie['sid']);
             setcookie('token', null, -1, '/');
             header('location: /login/');
