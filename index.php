74297a9a5a1cf73e09499661e3caec5dc72dce3c
Igor Garifyanov <garifianov.i@gmail.com>
2019-05-15 18:20:23 +0300

    set all to not nulllll

--- index.php
+++ index.php
@@ -535,8 +535,48 @@ if (isSurnameColumnNotNull())
     global $pdo;
     $sql = $pdo->prepare('alter table users modify surname text null');
     $sql->execute();
+}
+
+function isPointsColumnNotNull()
+{
+    global $pdo;
+    $sql = 'SHOW COLUMNS FROM `users`';
+    $sql = $pdo->prepare($sql);
+    $sql->execute();
+    $columns = $sql->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($columns as $column) {
+        if ($column['Field'] == 'points' && $column['Null'] == 'NO') {
+            return true;
+        }
+    }
+    return false;
+}
+
+if (isPointsColumnNotNull())
+{
+    global $pdo;
     $sql = $pdo->prepare('alter table users modify points text null');
     $sql->execute();
+}
+
+function isActivityColumnNotNull()
+{
+    global $pdo;
+    $sql = 'SHOW COLUMNS FROM `users`';
+    $sql = $pdo->prepare($sql);
+    $sql->execute();
+    $columns = $sql->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($columns as $column) {
+        if ($column['Field'] == 'activity' && $column['Null'] == 'NO') {
+            return true;
+        }
+    }
+    return false;
+}
+
+if (isActivityColumnNotNull())
+{
+    global $pdo;
     $sql = $pdo->prepare('alter table users modify activity text null');
     $sql->execute();
 }
