d6ecd7489a13efd211535be90fd8d218de869798
Igor Garifyanov <garifianov.i@gmail.com>
2019-05-21 18:13:21 +0300

    add links to log

--- engine/backend/other/log.php
+++ engine/backend/other/log.php
@@ -4,13 +4,22 @@ global $pdo;
 global $cometHash;
 global $cometTrackChannelName;
 
-$eventsQuery = $pdo->prepare('SELECT e.action, e.task_id, t.name AS taskName, e.author_id, u.name, u.surname, e.comment, c.comment AS commentText, e.datetime, e.view_status FROM events e
+$eventsQuery = $pdo->prepare('SELECT e.action, e.task_id, t.name AS taskName, e.author_id, u.name, u.surname, e.comment AS commentId, c.comment AS commentText, e.datetime, e.view_status FROM events e
   LEFT JOIN tasks t ON t.id = e.task_id
   LEFT JOIN users u on u.id = e.author_id
   LEFT JOIN comments c on c.id = e.comment                                                                              
   WHERE e.recipient_id = :userId');
 $eventsQuery->execute(array(':userId' =>$id));
 $events = $eventsQuery->fetchAll(PDO::FETCH_ASSOC);
+foreach ($events as &$event) {
+    $event['link'] = '';
+    if ($event['action'] == 'comment') {
+        $event['link'] = 'task/' . $event['task_id'] . '/#' . $event['commentId'];
+    } else {
+        $event['link'] = 'task/' . $event['task_id'] . '/';
+
+    }
+}
 
 $newtask = DBOnce('COUNT(*) as count','tasks','view="0" and status = "new" and worker='.$id);
 $overduetask = DBOnce('COUNT(*) as count','tasks','view="0" and status = "overdue" and worker='.$id);
