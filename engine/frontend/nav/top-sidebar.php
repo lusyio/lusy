6baf3e3e0dea37e06bbd63b59ab61b9a3cde25c0
Kelevra <kelevra@MacBook-Pro-Dmitrij-2.local>
2019-05-22 13:09:07 +0300

    top-sidebar

--- engine/frontend/nav/top-sidebar.php
+++ engine/frontend/nav/top-sidebar.php
@@ -2,43 +2,55 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-3">
-                <a class="navbar-brand text-uppercase font-weight-bold visible-lg" href="/"><?= $namec ?></a>
+                <a class="navbar-brand text-uppercase font-weight-bold visible-lg mt-1" href="/"><?= $namec ?></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"><i
                             class="fas fa-bars"></i></button>
             </div>
-            <div class="col-sm-4">
+            <div class="col-sm-5">
                 <form method="post" action="/../search/">
-                    <input class="form-control mr-sm-2" id="search" type="text" name="request" autocomplete="off"
-                           placeholder="Search..." style=" margin-left: 8px; ">
+                    <div class="form-group mb-0 mt-1">
+                        <div class="input-group">
+                            <div class="custom-file">
+                                <input class="form-control" id="search" type="text" name="request" autocomplete="off"
+                                       placeholder="<?=$_searchtext?>..." style=" margin-left: 8px; ">
+                            </div>
+                            <div class="input-group-append">
+                                <button class="input-group-text" id="searchButton"><i class="fas fa-search"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </form>
             </div>
-            <div class="col-sm-5">
+            <div class="col-sm-4">
                 <div class="float-right text-right alerts">
-                    <a href="/mail/" class="mr-3 text-decoration-none"><i
-                                class="fas fa-envelope <?= ($newMailCount) ? 'text-warning' : '' ?>"
-                                id="messagesIcon"></i> <strong class="text-warning"
-                                                               id="messagesCount"><?= ($newMailCount) ? $newMailCount : '' ?></strong></a>
-                    <a href="/log/" class="mr-3 text-decoration-none"><i
-                                class="fas fa-bell <?= ($newLogCount) ? 'text-warning' : '' ?>"
-                                id="notificationIcon"></i><strong class="text-warning"
-                                                                  id="notificationCount"><?= ($newLogCount) ? $newLogCount : '' ?></strong></a>
-                    <a href="/log/" class="mr-3 text-decoration-none"><i
-                                class="fas fa-comment  <?= ($newCommentCount) ? 'text-warning' : '' ?>"
-                                id="commentIcon"></i><strong class="text-warning"
-                                                             id="notificationCount"><?= ($newCommentCount) ? $newCommentCount : '' ?></strong></a>
-                    <!--                    <a href="/logout/" class="mr-3"><i class="fas fa-sign-out-alt"></i></a>-->
-                    <!--					<a href="/profile/-->
+                    <a href="/log/" class="mr-3 text-decoration-none">
+                        <i class="far fa-clipboard <?= ($newLogCount) ? 'text-primary' : '' ?>" id="notificationIcon"></i>
+                        <strong class="text-primary" id="notificationCount"><?= ($newLogCount) ? $newLogCount : '' ?></strong>
+                    </a>
+                    <a href="/log/" class="mr-3 text-decoration-none">
+                        <i class="fas fa-fire-alt <?= ($newOverdueCount) ? 'text-danger' : '' ?>" id="notificationIcon"></i>
+                        <strong class="text-danger" id="notificationCount"><?= ($newOverdueCount) ? $newOverdueCount : '' ?></strong>
+                    </a>
+                    <a href="/log/" class="mr-3 text-decoration-none">
+                        <i class="fas fa-comment  <?= ($newCommentCount) ? 'text-warning' : '' ?>" id="commentIcon"></i>
+                        <strong class="text-warning" id="notificationCount"><?= ($newCommentCount) ? $newCommentCount : '' ?></strong>
+                    </a>
+                    <a href="/mail/" class="mr-3 text-decoration-none">
+                        <i class="far fa-envelope <?= ($newMailCount) ? 'text-success' : '' ?>" id="messagesIcon"></i>
+                        <strong class="text-success" id="messagesCount"><?= ($newMailCount) ? $newMailCount : '' ?></strong>
+                    </a>
                     <? //=$id?><!--/"><img class="user-img rounded-circle" src="/upload/avatar/-->
                     <? //=$id?><!--.jpg"/></a>-->
-                    <span class="profile-menu-trigger" data-trigger="dropdown"><img
-                                class="user-img rounded-circle "
-                                src="/upload/avatar/<?= $id ?>.jpg"/></span>
+                    <span class="profile-menu-trigger" data-trigger="dropdown">
+                        <img class="user-img rounded-circle " src="/upload/avatar/<?= $id ?>.jpg"/>
+                    </span>
                     <div class="profile-submenu submenu">
-                        <a href="/profile/<?= $id ?>/">Профиль<i class="ml-2 fas fa-user-alt"></i></a>
-                        <a href="#">Журнал<i class="ml-2 fas fa-bell"></i></a>
-                        <a href="/settings/">Настройки<i class="ml-2 fas fa-cog"></i></a>
-                        <a href="/logout/">Выйти<i class="ml-2 fas fa-sign-out-alt"></i></a>
+                        <a href="/profile/<?= $id ?>/"><i class="mr-2 fas fa-user-alt"></i><?=$_profile?></a>
+                        <a href="/log/"><i class="mr-2 fas fa-bell"></i><?=$_history?></a>
+                        <a href="/settings/"><i class="mr-2 fas fa-cog"></i><?=$_settings?></a>
+                        <a href="/logout/"><i class="mr-2 fas fa-sign-out-alt"></i><?=$_logout?></a>
                     </div>
                 </div>
             </div>
