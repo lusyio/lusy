c515125628e47593d269e00b6cf4a53b4c1354d6
Front <dimkaFPS@gmail.com>
2019-05-23 13:43:57 +0300

    search container

--- engine/frontend/other/search.php
+++ engine/frontend/other/search.php
@@ -1,46 +1,65 @@
-<div id="searchResult">
-
-    <?php if (isset($request) && mb_strlen($request) > 0): ?>
-    <div>
-        <p>Результаты по запросу: <?= $request ?></p>
+<div class="search-container">
+    <div id="searchResult">
+        <?php if (isset($request) && mb_strlen($request) > 0): ?>
+        <div class="text-center text-reg mt">
+            <p class="pt-2">Результат по запросу &#128270: <?= $request ?></p>
+        </div>
     </div>
-
-</div>
-<div id="taskSearch" class="resultSet">
-    <p>Задачи(<?= count($result['task']); ?>)</p>
-    <?php $i = 1; ?>
-    <?php foreach ($result['task'] as $resultItem): ?>
-        <div class="result-item">
-            <a href="../task/<?= $resultItem['id'] ?>/"><?= $i ?> <?= $resultItem['name'] ?></a>
+    <div class="search-result">
+        <div class="card">
+            <div class="card-body">
+                <div id="taskSearch" class="resultSet">
+                    <p class="text-reg">Задачи (<?= count($result['task']); ?>)</p>
+                    <hr>
+                    <?php $i = 1; ?>
+                    <?php foreach ($result['task'] as $resultItem): ?>
+                        <a class="search-row" href="../task/<?= $resultItem['id'] ?>/">
+                            <div class="result-item">
+                                <span class="search-header"><?= $resultItem['name'] ?></span>
+<!--                                <br>-->
+<!--                                <span class="search-content">--><?//= $i ?><!--</span>-->
+                            </div>
+                        </a>
+                        <?php $i++; ?>
+                    <?php endforeach; ?>
+                </div>
+                <div id="fileSearch" class="resultSet">
+                    <p class="text-reg">Файлы (<?= count($result['file']); ?>)</p>
+                    <hr>
+                    <?php $i = 1; ?>
+                    <?php foreach ($result['file'] as $resultItem): ?>
+                        <a class="search-row" href="../<?= $resultItem['file_path'] ?>">
+                            <div class="result-item">
+                                <span class="search-header"><?= $resultItem['file_name'] ?></span>
+<!--                                <br>-->
+<!--                                <span class="search-content">--><?//= $i ?><!--</span>-->
+                            </div>
+                        </a>
+                        <?php $i++; ?>
+                    <?php endforeach; ?>
+                    <br>
+                </div>
+                <div id="commentSearch" class="resultSet">
+                    <p class="text-reg">Комментарии (<?= count($result['comment']); ?>)</p>
+                    <hr>
+                    <?php $i = 1; ?>
+                    <?php foreach ($result['comment'] as $resultItem): ?>
+                        <a class="search-row" href="../task/<?= $resultItem['idtask'] ?>/#"<?= $resultItem['id'] ?>>
+                            <div class="result-item">
+                                <span class="search-header"><?= $resultItem['comment'] ?></span>
+<!--                                <br>-->
+<!--                                <span class="search-content">--><?//= $i ?><!-- </span>-->
+                            </div>
+                        </a>
+                        <?php $i++; ?>
+                    <?php endforeach; ?>
+                </div>
+                <?php else: ?>
+                    <div class="search-empty">
+                        <p>По запросу ничего не найдено.</p>
+                    </div>
+                <?php endif; ?>
+            </div>
         </div>
-        <?php $i++; ?>
-    <?php endforeach; ?>
-    <br>
-</div>
-<div id="fileSearch" class="resultSet">
-    <p>Файлы(<?= count($result['file']); ?>)</p>
-    <?php $i = 1; ?>
-    <?php foreach ($result['file'] as $resultItem): ?>
-        <div class="result-item">
-            <a href="../<?= $resultItem['file_path'] ?>"><?= $i ?> <?= $resultItem['file_name'] ?></a>
-        </div>
-        <?php $i++; ?>
-    <?php endforeach; ?>
-    <br>
-</div>
-<div id="commentSearch" class="resultSet">
-    <p>Комментарии(<?= count($result['comment']); ?>)</p>
-    <?php $i = 1; ?>
-    <?php foreach ($result['comment'] as $resultItem): ?>
-        <div class="result-item">
-            <a href="../task/<?= $resultItem['idtask'] ?>/#"<?= $resultItem['id'] ?>><?= $i ?> <?= $resultItem['comment'] ?></a>
-        </div>
-        <?php $i++; ?>
-    <?php endforeach; ?>
-</div>
-<?php else: ?>
-    <div>
-        <p>Не задан запрос</p>
     </div>
-<?php endif; ?>
 </div>
