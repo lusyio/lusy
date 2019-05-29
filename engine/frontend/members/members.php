79c08a924c8573ff804388aece6493413edd1afd
Front <dimkaFPS@gmail.com>
2019-05-29 13:41:07 +0300

    Поправка members

--- engine/frontend/members/members.php
+++ engine/frontend/members/members.php
@@ -6,6 +6,51 @@
             <a href="#"><?= $task['managerName'] ?> <?= $task['managerSurname'] ?></a>
         </div>
         <hr class="m-0">
+        <div class="members-responsible">
+            <div class="row">
+                <div class="col-8 text-justify">
+                    <span>Ответственный</span>
+                </div>
+                <div class="col text-right">
+                    <i class="fas fa-pencil-alt add-coworker editResponsible" data-toggle="collapse"
+                       data-target="#responsibleList" aria-expanded="false" aria-controls="responsibleList"></i>
+                </div>
+            </div>
+            <hr class="mt-0 mb-1">
+            <div class="members-responsible-one mb-1">
+                <div class="row">
+                    <div class="col-1">
+                        <img title="<?= $viewStatusTitle ?>" src="/<?= getAvatarLink($coworker['worker_id']) ?>"
+                             class="avatar-added mr-1">
+                    </div>
+                    <div class="col text-justify">
+                        <a href="#"><?= $coworker['name'] ?> <?= $coworker['surname'] ?></a>
+                    </div>
+                </div>
+            </div>
+            <hr class="m-0">
+            <div class="p-1 text-justify collapse" id="responsibleList">
+                <?php
+                $users = DB('*', 'users', 'idcompany=' . $GLOBALS["idc"]);
+                foreach ($users as $n) { ?>
+                    <div class="responsible-one">
+                        <div class="row">
+                            <div class="col-1">
+                                <img src="/<?= getAvatarLink($n['id']) ?>" class="avatar-added mr-1">
+                            </div>
+                            <div class="col">
+                                <a href="#" class="mb-1 add-coworker-text"><?php echo $n['name'] . ' ' . $n['surname'] ?></a>
+                            </div>
+                            <div class="col-2">
+                                <i value="<?php echo $n['id'] ?>"
+                                   class="fas fa-exchange-alt add-coworker changeResponsible"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <hr class="m-0">
+                <?php } ?>
+            </div>
+        </div>
         <div class="members-coworkers">
             <div class="row">
                 <div class="col-8 text-justify">
@@ -58,7 +103,6 @@
             </div>
             <div class="mt-3 text-center">
                 <button class="btn btn-success btn-sm" id="confirmMembers" type="button">Принять</button>
-                <!--                <button class="btn btn-outline-danger btn-sm" id="cancelMembers" type="button">Отмена</button>-->
             </div>
         </div>
     </div>
