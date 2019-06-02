bd2b9d5e3d63e0aa5ec3c7ef3f1fc7fdfb6bf6d5
Kelevra <kelevra@MacBook-Pro-Dmitrij-2.local>
2019-06-02 21:13:35 +0300

    Сдвинул лог на край div

--- engine/frontend/other/task-card.php
+++ engine/frontend/other/task-card.php
@@ -9,24 +9,21 @@
                             <div class="d-flex">
                                 <div class="mr-3">
                                     <span class="taskname"><?= ($isTaskRead)?'':'<span class="text-danger font-weight-bold mr-1">!</span>'; ?><?= $n['name'] ?></span>
-                                </div>
-                                <div class="d-flex fc">
-                                    <div class="informer d-flex mr-3 <?= ($hasNewComments)?'text-primary':''; ?>"><i class="fas fa-comments">
-                                        </i><span class="ml-1"><?=$n['countcomments']?></span>
-                                    </div>
-                                    <div class="informer d-flex">
-                                        <i class="fas fa-file"></i><span class="ml-1"><?=$n['countAttachedFiles']?></span>
+                                    <div style=" color: #c6c9dc; font-size: 12px;    margin-top: 5px; ">
+                                        <span class="mr-3">#<?= $n['idtask'] ?></span>
+                                        <span class="mr-3"><i class="fas fa-comments"></i><span class="ml-1"><?=$n['countcomments']?></span></span>
+                                        <span class="mr-3"><i class="fas fa-file"></i><span class="ml-1"><?=$n['countAttachedFiles']?></span></span>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-sm-2 col-5">
+                        <div class="col-sm-2 col-5" style=" margin-top: 10px; ">
                             <?= $taskStatusText[$n['mainRole']][$n['status']] ?>
                         </div>
-                        <div class="col-sm-2 col-3 <?= ($n['status']=='overdue')?'text-danger font-weight-bold':''; ?> <?= ($n['status']=='inwork' and $n['datedone']==$now)?'text-warning font-weight-bold':''; ?>">
+                        <div style=" margin-top: 10px; " class="col-sm-2 col-3 <?= ($n['status']=='overdue')?'text-danger font-weight-bold':''; ?> <?= ($n['status']=='inwork' and $n['datedone']==$now)?'text-warning font-weight-bold':''; ?>">
                             <?= $n['deadLineDay'] ?> <?= $n['deadLineMonth'] ?>
                         </div>
-                        <div class="col-sm-2 col-4 avatars">
+                        <div class="col-sm-2 col-4 avatars" style=" margin-top: 10px; ">
                             <div>
                                 <img src="/<?=getAvatarLink($n['idmanager'])?>" title="<?= $viewStatusTitleManager ?>" class="avatar"> |
                                 <img src="/<?=getAvatarLink($n['idworker'])?>" title="<?= $viewStatusTitleManager ?>" class="avatar">
