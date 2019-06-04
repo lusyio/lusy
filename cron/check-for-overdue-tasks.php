a4f1c3b9c8e87ac66f38cbf3e21e3646368267f2
Igor Garifyanov <garifianov.i@gmail.com>
2019-06-04 12:58:00 +0300

    Доработал крон

--- cron/check-for-overdue-tasks.php
+++ cron/check-for-overdue-tasks.php
@@ -6,4 +6,3 @@ include __DIR__ . '/../conf.php'; // подключаем базу данных
 
 $overdueTasksQuery = $pdo->prepare('UPDATE tasks SET status = :newStatus WHERE status = :oldStatus AND ((ISNULL(datepostpone) AND datedone < :nowTime) OR (datepostpone < :nowTime))');
 $overdueTasksQuery->execute(array(':newStatus' => 'overdue',':oldStatus' => 'inwork','nowTime' => time()-86400));
-var_dump($pdo->errorInfo());
