343e47760a96bc56ae5b1113e2fc9f438e365c6b
Igor Garifyanov <garifianov.i@gmail.com>
2019-06-11 11:47:19 +0300

    логаут

--- engine/backend/other/logout.php
+++ engine/backend/other/logout.php
@@ -2,7 +2,7 @@
 require_once 'engine/backend/functions/login-functions.php';
 $sessionCookie = parseCookie($_COOKIE['token']);
 removeSessions($sessionCookie['sid']);
-//setcookie('token', null, -1, '/');
+setcookie('token', null, -1, '/');
 unset($_COOKIE[session_name()]);
 unset($_SESSION);
 //session_destroy();
