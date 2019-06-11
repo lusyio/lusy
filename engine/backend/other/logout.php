536325b28784a776c5702592ed4ba2c7b3c4e039
Igor Garifyanov <garifianov.i@gmail.com>
2019-06-11 12:05:39 +0300

    Update logout.php

--- engine/backend/other/logout.php
+++ engine/backend/other/logout.php
@@ -5,8 +5,7 @@ $sessionCookie = parseCookie($_COOKIE['token']);
 removeSessions($sessionCookie['sid']);
 setcookie('token', null, -1, '/');
 unset($_COOKIE[session_name()]);
-unset($_SESSION);
-session_destroy();
+unset($_SESSION['auth']);
 header('location: /login/');
 ob_end_flush();
 ?>
