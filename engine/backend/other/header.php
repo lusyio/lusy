349d3d16c0e7af7b10615e0c4f243c12cfb6c367
Igor Garifyanov <garifianov.i@gmail.com>
2019-06-17 21:36:34 +0300

    Добавил кэширование аватарок

--- engine/backend/other/header.php
+++ engine/backend/other/header.php
@@ -36,11 +36,20 @@ if ($isAuthorized) {
         }
         if ($_GET['avatar'] == $_SESSION['idc']) {
             $file = 'upload/avatar/' . $_GET['avatar'] . '/' . $_GET['name'] . '.jpg';
-            header('Content-type: image/jpeg');
-            header("Cache-Control: private");
-            header("Cache-Control: max-age=2592000");
-            readfile($file);
-            die();
+            $last_modified_time = filemtime($file);
+            $etag = md5_file($file);
+            header("Last-Modified: ".gmdate("D, d M Y H:i:s", $last_modified_time)." GMT");
+            header("Etag: $etag");
+            if (@strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) == $last_modified_time || @trim($_SERVER['HTTP_IF_NONE_MATCH']) == $etag) {
+                header("HTTP/1.1 304 Not Modified");
+                exit;
+            } else {
+                header('Content-type: image/jpeg');
+                header("Cache-Control: private, max-age=2592000");
+                readfile($file);
+                ob_flush();
+                die();
+            }
         } else {
             header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
             die();
