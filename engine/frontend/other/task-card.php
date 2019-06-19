60de7e9cfaf2278f9e1e7b33e6c355992183aaa4
Kelevra <kelevra@MacBook-Pro-Dmitrij-2.local>
2019-06-19 20:16:31 +0300

    Верстка индикаторов файлы/комменты в Tasks

--- engine/frontend/other/task-card.php
+++ engine/frontend/other/task-card.php
@@ -5,31 +5,34 @@
                 <div class="d-block border-left-tasks <?= $borderColor[$n['status']] ?> ">
                     <p class="font-weight-light text-ligther d-none"><?= $taskStatusText[$n['mainRole']][$n['status']] ?></p>
                     <div class="row">
-                        <div class="col-sm-6 col-12">
-                            <div class="d-flex">
-                                <div class="mr-3">
-                                    <span class="taskname"><?= ($isTaskRead)?'':'<span class="text-danger font-weight-bold mr-1">!</span>'; ?><?= $n['name'] ?></span>
+                        <div class="col-sm-5 col-12">
+                            <div>
+                                <span class="taskname"><?= ($isTaskRead) ? '' : '<span class="text-danger font-weight-bold mr-1">!</span>'; ?><?= $n['name'] ?></span>
+                            </div>
+                        </div>
+                        <div class="col-sm-1 pl-0">
+
+                            <div class="d-flex fc">
+                                <div class="informer d-flex mr-3 <?= ($hasNewComments)?'text-primary':''; ?>"><i class="fas fa-comments">
+                                    </i><span class="ml-1"><?=$n['countcomments']?></span>
                                 </div>
-                                <div class="d-flex fc">
-                                    <div class="informer d-flex mr-3 <?= ($hasNewComments)?'text-primary':''; ?>"><i class="fas fa-comments">
-                                        </i><span class="ml-1"><?=$n['countcomments']?></span>
-                                    </div>
-                                    <div class="informer d-flex">
-                                        <i class="fas fa-file"></i><span class="ml-1"><?=$n['countAttachedFiles']?></span>
-                                    </div>
+                                <div class="informer d-flex">
+                                    <i class="fas fa-file"></i><span class="ml-1"><?=$n['countAttachedFiles']?></span>
                                 </div>
                             </div>
                         </div>
                         <div class="col-sm-2 col-5">
                             <?= $taskStatusText[$n['mainRole']][$n['status']] ?>
                         </div>
-                        <div class="col-sm-2 col-3 <?= ($n['status']=='overdue')?'text-danger font-weight-bold':''; ?> <?= ($n['status']=='inwork' and date("Y-m-d", $n['datedone']) == $now)?'text-warning font-weight-bold':''; ?>">
+                        <div class="col-sm-2 col-3 <?= ($n['status'] == 'overdue') ? 'text-danger font-weight-bold' : ''; ?> <?= ($n['status'] == 'inwork' and date("Y-m-d", $n['datedone']) == $now) ? 'text-warning font-weight-bold' : ''; ?>">
                             <?= $n['deadLineDay'] ?> <?= $n['deadLineMonth'] ?>
                         </div>
                         <div class="col-sm-2 col-4 avatars">
                             <div>
-                                <img src="/<?=getAvatarLink($n['idmanager'])?>" title="<?= $viewStatusTitleManager ?>" class="avatar"> |
-                                <img src="/<?=getAvatarLink($n['idworker'])?>" title="<?= $viewStatusTitleManager ?>" class="avatar">
+                                <img src="/<?= getAvatarLink($n['idmanager']) ?>" title="<?= $viewStatusTitleManager ?>"
+                                     class="avatar"> |
+                                <img src="/<?= getAvatarLink($n['idworker']) ?>" title="<?= $viewStatusTitleManager ?>"
+                                     class="avatar">
                             </div>
                         </div>
                     </div>
