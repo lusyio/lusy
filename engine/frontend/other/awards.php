4e6a844c82d2604cb5a96c96c4da3cce7b667645
Front <dimkaFPS@gmail.com>
2019-06-20 16:33:54 +0300

    скрол для ачивок

--- engine/frontend/other/awards.php
+++ engine/frontend/other/awards.php
@@ -328,6 +328,25 @@
 </div>
 
 <script>
+    jQuery(function ($) {
+        $.fn.hScroll = function (amount) {
+            amount = amount || 120;
+            $(this).bind("DOMMouseScroll mousewheel", function (event) {
+                var oEvent = event.originalEvent,
+                    direction = oEvent.detail ? oEvent.detail * -amount : oEvent.wheelDelta,
+                    position = $(this).scrollLeft();
+                position += direction > 0 ? -amount : amount;
+                $(this).scrollLeft(position);
+                event.preventDefault();
+            })
+        };
+    });
+
+    $(document).ready(function(){
+        $('.award-container').hScroll(30);
+    });
+
+
     $('.circle').circleProgress({
         size: 75,
     });
