094a7c0a525b83a5ec4454814b27ca6fd7cf9210
Kelevra <kelevra@MacBook-Pro-Dmitrij-2.local>
2019-06-27 16:28:13 +0300

    Правки чата

--- engine/frontend/other/message.php
+++ engine/frontend/other/message.php
@@ -2,10 +2,10 @@
      class="rounded-0 message <?= ($message['owner']) ? 'my-message' : 'not-my-message'; ?> <?= ($message['view_status'] || $message['sender'] == $id) ? '' : 'alert-primary' ?>">
     <div class="row">
         <div class="col-2 col-lg-1">
-            <img src="/<?= getAvatarLink($message['sender']) ?>" class="avatar-conversation">
+            <a href="/profile/<?=$message['sender']?>/"><img src="/<?= getAvatarLink($message['sender']) ?>" class="avatar-conversation"></a>
         </div>
         <div class="col pl-2 message-width">
-            <p class="m-0"><?= $message['author'] ?> <span class="d-none"><?= $message['status'] ?></span> <span
+            <p class="m-0 mb-1"><a href="/profile/<?=$message['sender']?>/" class="text-dark btn p-0 border-0 font-weight-bold"><?= $message['author'] ?></a> <span class="d-none"><?= $message['status'] ?></span> <span
                         class="date mr-2"><?= date('d.m H:i', $message['datetime']) ?></span></p>
             <p class="m-0"><?= link_it(nl2br(htmlspecialchars($message['mes']))) ?></p>
             <?php if (count($message['files']) > 0): ?>
