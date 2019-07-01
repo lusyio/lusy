37de353d592ae93833773e31cd067f062b8ee892
Front <dimkaFPS@gmail.com>
2019-07-01 17:39:28 +0300

    ресторе пасворд новое оформление

--- engine/frontend/other/restore-password.php
+++ engine/frontend/other/restore-password.php
@@ -1,30 +1,57 @@
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-12 col-md-5 col-xl-4 my-5">
-            <!-- Heading -->
-            <h1 class="display-4 text-center mb-3">
-                Смена пароля
-            </h1>
-            <!-- Form -->
-            <form action="" method="POST">
-                <!-- Password -->
-                <div class="form-group">
-                    <!-- Label -->
-                    <label>
-                        Password
-                    </label>
-                    <!-- Input group -->
-                    <div class="input-group input-group-merge">
-                        <!-- Input -->
-                        <input type="password" name="password" class="form-control form-control-appended" placeholder="Enter your password" required>
+<div class="join-page">
+    <div class="container pt-5">
+        <h1 class="text-white lead-text display-4 text-center mt-5 mb-3">
+            Смена пароля
+        </h1>
+        <p class="lead-text-p text-center">Укажите новый пароль</p>
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-5 col-xl-4 mt-3">
+                <form action="" method="POST">
+                    <!-- Password -->
+                    <div class="form-group">
+                        <!-- Label -->
+                        <!-- Input group -->
+                        <div class="input-group input-group-merge">
+                            <!-- Input -->
+                            <input type="password" name="password" class="form-control form-control-appended"
+                                   placeholder="Введите новый пароль" required>
+                        </div>
                     </div>
-                </div>
-                <!-- Submit -->
-                <button class="btn btn-lg btn-block btn-primary mb-3">
-                    Сменить пароль
-                </button>
-            </form>
+                    <!-- Submit -->
+                    <button class="btn btn-lg btn-block btn-primary mb-3">
+                        Сменить пароль
+                    </button>
+                </form>
 
-        </div>
-    </div> <!-- / .row -->
-</div> <!-- / .container -->
+            </div>
+        </div> <!-- / .row -->
+    </div> <!-- / .container -->
+</div>
+<p class="text-center position-absolute" style="left: 0; right: 0; bottom: 0"><a href="https://lusy.io/">LUSY.IO</a></p>
+<style>
+    html, body {
+        height: 100%;
+    }
+
+    .form-control {
+        height: calc(2.5em + .75rem + 2px) !important;
+        border-radius: 15px !important;
+        padding-left: 20px;
+    }
+
+    .btn-primary {
+        background: #284c8e;
+        border: none !important;
+        border-radius: 15px;
+        padding-left: 25px !important;
+        padding-right: 25px !important;
+    }
+
+    .btn-primary:hover {
+        background: #13387c;
+    }
+
+    #btn-show-restore-form {
+        color: #e1e1e1;
+    }
+</style>
