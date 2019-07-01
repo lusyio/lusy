1187939c24ae55dcecd28a29865cbfe79c00f223
Igor Garifyanov <garifianov.i@gmail.com>
2019-07-01 10:12:25 +0300

    Убрал ссылку на системного пользователя из лога

--- engine/frontend/event-messages/comment.php
+++ engine/frontend/event-messages/comment.php
@@ -6,7 +6,11 @@
     <div class="position-relative">
         <span class="date"><?= date("d.m H:i", $event['datetime']); ?></span>
         <img src="/<?=getAvatarLink($event['author_id'])?>" class="avatar mr-2">
-        <a href="/profile/<?=$event['author_id']?>/" class="font-weight-bold"><?= $event['name'] ?> <?= $event['surname'] ?></a>
+        <?php if ($event['author_id'] == 1): ?>
+            <a class="font-weight-bold"><?= $event['name'] ?> <?= $event['surname'] ?></a>
+        <?php else: ?>
+            <a href="/profile/<?=$event['author_id']?>/" class="font-weight-bold"><?= $event['name'] ?> <?= $event['surname'] ?></a>
+        <?php endif; ?>
     </div>
     <p class="mt-2 mb-2"><?= $GLOBALS['_logNewComment'] ?> <a href="/../<?= $event['link'] ?>" class="font-italic">"<?=$event['taskname']?>"</a></p>
         <div class="commentText p-2 pl-3"><?= $event['commentText'] ?></div>
