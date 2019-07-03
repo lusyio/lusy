eb5185d7c41bda7e1ba604afde8534ac7a243291
Front <dimkaFPS@gmail.com>
2019-07-03 16:54:26 +0300

    смена оформления мемберс, отступы всякие

--- engine/frontend/task/control/manager/postpone.php
+++ engine/frontend/task/control/manager/postpone.php
@@ -1,8 +1,3 @@
-
-
-<link href="/assets/css/datepicker.min.css" rel="stylesheet" type="text/css">
-
-
 <div id="status-block">
     <div class="postpone-manager">
         <p class="text-ligther">
@@ -12,14 +7,10 @@
             <h5>Причина:</h5>
             <?=htmlspecialchars_decode($request)?>
         </div>
-        <div class="pl-2">
-            <i class="fas fa-check custom-date accept" id="confirmDate"></i><i class="fas fa-times custom-date cancel" id="cancelDate"></i>
+        <div>
+            <i data-toggle="tooltip" data-placement="bottom" title="Принять перенос" class="fas fa-check custom-date accept" id="confirmDate"></i><i data-toggle="tooltip" data-placement="bottom" title="Отклонить перенос" class="fas fa-times custom-date cancel" id="cancelDate"></i>
         </div>
     </div>
     <button id="workdone" type="button" class="btn btn-outline-primary mt-3 mb-1"><i class="fas fa-check mr-2"></i> <?=$GLOBALS["_completetask"]?></button>
     <button id="cancelTask" type="button" class="btn btn-outline-danger mt-3 mb-1"><i class="fas fa-times cancel mr-2" id="cancel-icon-button"></i> <?=$GLOBALS["_cancel"]?></button>
 </div>
-
-
-<script>
-</script>
