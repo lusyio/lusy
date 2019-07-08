cd5143e191c346ff297eaad8d20340cfe46546d6
Igor Garifyanov <garifianov.i@gmail.com>
2019-07-08 17:23:23 +0300

    Увеличил лимит размера загружаемого файла

--- engine/backend/functions/common-functions.php
+++ engine/backend/functions/common-functions.php
@@ -25,7 +25,7 @@ function uploadAttachedFiles($type, $eventId)
         return;
     }
 
-    $maxFileSize = 20 * 1024 * 1024;
+    $maxFileSize = 500 * 1024 * 1024;
     $types = ['task', 'comment', 'conversation', 'chat'];
     if (!in_array($type, $types)) {
         return;
