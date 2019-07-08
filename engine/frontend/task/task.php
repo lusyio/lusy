2e30d878b86f40a99df34c26f678ea2024f98efb
Front <dimkaFPS@gmail.com>
2019-07-08 14:52:57 +0300

    поправил условие для переноса дедлайна

--- engine/frontend/task/task.php
+++ engine/frontend/task/task.php
@@ -136,7 +136,7 @@ if ($id == $worker and $view == 0) {
                                 </div>
                             </medium>
                         </div>
-                        <?php if (!$isCeo && !$isCoworker): ?>
+                        <?php if (!$isCeo || !$isCoworker): ?>
                         <span class="position-absolute edit"><i class="fas fa-pencil-alt"></i></span>
                         <div id="change-date">
                             <div class="form-group mb-0 p-3">
