a0c537747ae30057e5acc9dfff4043112fc31751
Igor Garifyanov <garifianov.i@gmail.com>
2019-07-09 09:43:31 +0300

    Поправил ссылку на комментарий в письме

--- engine/phpmailer/templates/ru/comment.php
+++ engine/phpmailer/templates/ru/comment.php
@@ -1,5 +1,5 @@
 <div class="block center" style="padding: 20px;text-align: center;">
     <h1>Новый комментарий</h1>
     <p class="margin20t" style="margin-top: 20px;line-height: 2;">{$authorName} оставил комментарий к задаче - {$taskName}</p>
-    <p class="margin30t" style="margin-top: 30px;line-height: 2;"><a class="button" href="https://s.lusy.io/task/{$taskId}#{$commentId}" style="background-color: #5a93df;border-color: #5a93df;color: #fff;font-weight: 400;text-align: center;vertical-align: middle;border: 1px solid transparent;padding: .375rem .75rem;font-size: 1rem;line-height: 1.5;border-radius: .25rem;text-decoration: none;">Перейти к комментарию</a></p>
+    <p class="margin30t" style="margin-top: 30px;line-height: 2;"><a class="button" href="https://s.lusy.io/task/{$taskId}/#{$commentId}" style="background-color: #5a93df;border-color: #5a93df;color: #fff;font-weight: 400;text-align: center;vertical-align: middle;border: 1px solid transparent;padding: .375rem .75rem;font-size: 1rem;line-height: 1.5;border-radius: .25rem;text-decoration: none;">Перейти к комментарию</a></p>
 </div>
