98999307adf77b8e6a3a0dc0c8176bfb3c21789d
Igor Garifyanov <garifianov.i@gmail.com>
2019-07-10 15:38:29 +0300

    Поправил событие Списание

--- engine/backend/functions/payment-functions.php
+++ engine/backend/functions/payment-functions.php
@@ -217,7 +217,7 @@ function addWithdrawalEvent($companyId, $orderId, $amount, $comment)
 {
     global $pdo;
     $addEventQuery = $pdo->prepare("INSERT INTO finance_events (event, event_datetime, company_id, orderId, amount, comment) VALUES 
-(:event, ':datetime', ':companyId', ':orderId', ':amount', ':comment')");
+(:event, :datetime, :companyId, :orderId, :amount, :comment)");
     $queryData = [
         ':event' => 'withdrawal',
         ':datetime' => time(),
