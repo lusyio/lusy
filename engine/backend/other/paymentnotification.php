89cf3582221502fc64e5130f14156fae1e2a0751
Igor Garifyanov <garifianov.i@gmail.com>
2019-07-12 15:52:28 +0300

    Update paymentnotification.php

--- engine/backend/other/paymentnotification.php
+++ engine/backend/other/paymentnotification.php
@@ -4,6 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 ob_start();
 
+require_once __ROOT__ . '/engine/backend/functions/common-functions.php';
 require_once __ROOT__ . '/engine/backend/functions/payment-functions.php';
 require_once __ROOT__ . '/engine/backend/other/TinkoffMerchantAPI.php';
 
