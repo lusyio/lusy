664f402f79cea7655d36997799666d67b15b5dde
Front <dimkaFPS@gmail.com>
2019-07-16 17:57:40 +0300

    Update tasknew.php

--- engine/frontend/other/tasknew.php
+++ engine/frontend/other/tasknew.php
@@ -147,11 +147,11 @@
                             <label class="label-responsible text-left">
                                 Надзадача
                             </label>
-<!--                            <span class="position-absolute disabledBtnOptions"-->
-<!--                                  style="background-color: #000;width: 100%;bottom: 2px; height: 50%;z-index: 100000;opacity: 0;">-->
-<!---->
-<!--                            </span>-->
-                            <div class="container container-subtask border-0 d-flex flex-wrap align-content-sm-stretch card-body-tasknew"
+                            <span class="position-absolute disabledBtnOptions"
+                                  style="background-color: #000;width: 100%;bottom: 2px; height: 50%;z-index: 100000;opacity: 0;">
+
+                            </span>
+                            <div class="container container-subtask border-0 d-flex flex-wrap align-content-sm-stretch card-body-tasknew disabled"
                                  style="min-height: 38px;padding-top: 10px;">
                                 <div class="placeholder-subtask">Не выбрана</div>
                                 <?php
