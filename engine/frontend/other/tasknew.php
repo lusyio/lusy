a815484c78f6108e6b0ec6978f393a8404e8c1c6
Front <dimkaFPS@gmail.com>
2019-07-17 12:17:07 +0300

    отключил работу надзадач для бесплатного

--- engine/frontend/other/tasknew.php
+++ engine/frontend/other/tasknew.php
@@ -164,26 +164,16 @@ $borderColor = [
                 <div class="row">
                     <div class="col-12 col-lg-8 top-block-tasknew top-block-tasknew">
                         <div class="card card-tasknew">
-                            <?php
-                            include __ROOT__ . '/engine/frontend/members/subtask.php';
-                            ?>
                             <label class="label-responsible text-left">
                                 Надзадача
                             </label>
-<!--                            <span class="position-absolute disabledBtnOptions"-->
-<!--                                  style="background-color: #000;width: 100%;bottom: 2px; height: 50%;z-index: 100000;opacity: 0;">-->
-<!---->
-<!--                            </span>-->
-                            <div class="container container-subtask border-0 d-flex flex-wrap align-content-sm-stretch card-body-tasknew "
+                            <span class="position-absolute disabledBtnOptions"
+                                  style="background-color: #000;width: 100%;bottom: 2px; height: 50%;z-index: 100000;opacity: 0;">
+
+                            </span>
+                            <div class="container container-subtask border-0 d-flex flex-wrap align-content-sm-stretch card-body-tasknew disabled"
                                  style="min-height: 38px;padding-top: 10px;">
                                 <div class="placeholder-subtask">Не выбрана</div>
-                                <?php
-                                foreach ($parentTasks as $parentTask) { ?>
-                                    <div val="<?php echo $parentTask['id']; ?>" class="add-subtask d-none border-left-tasks <?= $borderColor[$parentTask['status']] ?>">
-                                        <span class="card-coworker"><?= mb_strimwidth($parentTask['name'], 0, 38, "..."); ?></span>
-                                    </div>
-                                    <hr class="m-0">
-                                <?php } ?>
                                 <div class="position-absolute icon-newtask icon-newtask-change-subtask">
                                     <i class="fas fa-caret-down"></i>
                                 </div>
