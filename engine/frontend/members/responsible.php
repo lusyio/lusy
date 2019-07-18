3d291b9543b7300439dbfd8db81caf9b56c16aa2
Front <dimkaFPS@gmail.com>
2019-07-18 11:10:30 +0300

    добавил место для фио в селектах

--- engine/frontend/members/responsible.php
+++ engine/frontend/members/responsible.php
@@ -8,10 +8,10 @@
                     <div class="col-2">
                         <img src="/<?= getAvatarLink($n["id"]) ?>" class="avatar-added mr-1">
                     </div>
-                    <div class="col text-left">
+                    <div class="col text-left pr-0">
                         <span class="mb-1 add-coworker-text"><?= (trim($n['name'] . ' ' . $n['surname']) == '') ? $n['email'] : trim($n['name'] . ' ' . $n['surname']) ?></span>
                     </div>
-                    <div class="col-2">
+                    <div class="col-2 pl-0">
                         <i class="fas fa-exchange-alt icon-change-responsible"></i>
                     </div>
                 </div>
--- engine/frontend/members/subtask.php
+++ engine/frontend/members/subtask.php
@@ -7,10 +7,10 @@
         foreach ($parentTasks as $parentTask) { ?>
             <div val="<?php echo $parentTask['id']; ?>" class="select-subtask">
                 <div class="row">
-                    <div class="col text-left text-area-message ml-2 border-left-tasks <?= $borderColor[$parentTask['status']] ?>">
+                    <div class="col pr-0 text-left text-area-message ml-2 border-left-tasks <?= $borderColor[$parentTask['status']] ?>">
                         <span class="mb-1 add-coworker-text"><?= $parentTask['name']; ?></span>
                     </div>
-                    <div class="col-2">
+                    <div class="col-2 pl-0">
                         <i class="fas fa-exchange-alt icon-change-responsible"></i>
                     </div>
                 </div>
