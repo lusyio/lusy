bb52154884e009355f80a0a48d2b0a798b7a6fd8
Igor Garifyanov <garifianov.i@gmail.com>
2019-07-19 12:27:30 +0300

    Доработал поддержку пользователей

--- engine/backend/other/support.php
+++ engine/backend/other/support.php
@@ -16,23 +16,28 @@ if ($idc != 1) {
 }
 
 $supportPage = true;
-$messages = DB('*','mail','recipient = 1 ORDER BY `datetime` DESC');
-$userList = [];
+$usersQuery = $pdo->prepare("SELECT DISTINCT u.id, u.idcompany AS companyId, u.name, u.surname, u.email, c.idcompany AS companyName, IF(m.view_status = 0 AND m.recipient = 1, 0, 1) AS supportViewStatus FROM mail m LEFT JOIN users u ON (m.sender <> 1 AND m.sender = u.id OR m.recipient <> 1 AND m.recipient = u.id) LEFT JOIN company c ON u.idcompany = c.id WHERE m.sender = 1 OR m.recipient = 1 ORDER BY supportViewStatus, m.datetime DESC");
+$usersQuery->execute();
+$users = $usersQuery->fetchAll(PDO::FETCH_ASSOC);
 
-$onlineUsersList = [];
-
-$dialog = [];
-foreach ($messages as $n) {
-    if (in_array($n['sender'], $dialog)) {
-    } else {
-        if ($n['sender'] != 1) {
-            $dialog[] = $n['sender'];
-        }
-    }
-    if (in_array($n['recipient'], $dialog)) {
-    } else {
-        if ($n['recipient'] != 1) {
-            $dialog[] = $n['recipient'];
-        }
-    }
+$lastMessageQuery = $pdo->prepare("SELECT message_id, mes, sender, recipient, view_status, datetime FROM mail WHERE (sender = :userId AND recipient = 1) OR(sender = 1 AND recipient = :userId) ORDER BY datetime DESC LIMIT 1");
+$newMessagesQuery = $pdo->prepare("SELECT COUNT(*) AS count FROM mail WHERE sender = :userId AND recipient = 1 AND view_status = 0");
+$messagesGroupedByCompany = [];
+foreach ($users as $user) {
+    $lastMessageQuery->execute([':userId' => $user['id']]);
+    $newMessagesQuery->execute([':userId' => $user['id']]);
+    $messagesGroupedByCompany[$user['companyId']]['companyName'] = $user['companyName'];
+    $messagesGroupedByCompany[$user['companyId']]['dialogs'][$user['id']]['message'] = $lastMessageQuery->fetch(PDO::FETCH_ASSOC);
+    $messagesGroupedByCompany[$user['companyId']]['dialogs'][$user['id']]['newMessages'] = $newMessagesQuery->fetch(PDO::FETCH_ASSOC)['count'];
+    $messagesGroupedByCompany[$user['companyId']]['dialogs'][$user['id']]['name'] = $user['name'];
+    $messagesGroupedByCompany[$user['companyId']]['dialogs'][$user['id']]['surname'] = $user['surname'];
+    $messagesGroupedByCompany[$user['companyId']]['dialogs'][$user['id']]['email'] = $user['email'];
 }
+$newMessagesByCompanyQuery = $pdo->prepare("SELECT COUNT(DISTINCT u.id) AS count FROM mail m LEFT JOIN users u ON m.sender = u.id WHERE u.idcompany = :companyId AND m.recipient = 1 AND m.view_status = 0");
+foreach ($messagesGroupedByCompany as $companyId => $companyMessages) {
+    $newMessagesByCompanyQuery->execute([':companyId' => $companyId]);
+    $messagesGroupedByCompany[$companyId]['newMessages'] = $newMessagesByCompanyQuery->fetch(PDO::FETCH_ASSOC)['count'];
+}
+
+$userList = [];
+$onlineUsersList = [];
--- engine/frontend/other/support.php
+++ engine/frontend/other/support.php
@@ -35,42 +35,62 @@
                     <?php } ?>
                 </div>
             </div>
-
         </div>
-        <?php
-        foreach ($dialog as $n) {
-            $lastMessage = lastmess($n, 1);
-            $newMessages = numberOfNewMessages($n, 1);
-            $isOnline = in_array($n, $onlineUsersList); ?>
-            <a class="text-decoration-none text-dark" href="./<?= $n ?>/">
+        <?php foreach ($messagesGroupedByCompany as $companyId => $companyMessages): ?>
+            <div class="company-support">
+            <a class="company-support-link text-decoration-none text-dark" href="#">
                 <div class="card mb-3 dialog-mail">
                     <div class="card-body">
                         <div class="row">
-                            <div class="col-2 pl-2">
-                                    <div class="user-pic position-relative" style="width:60px">
-                                        <img src="/<?= getAvatarLink($n) ?>"
-                                             class="avatar-img rounded-circle w-100"/>
-                                        <span class="online-indicator mobile-online-indicator">
-                                            <i class="fas fa-circle mr-1 ml-1 onlineIndicator mail <?= ($isOnline) ? 'text-success' : '' ?>"></i>
-                                        </span>
-                                    </div>
-                            </div>
-                            <div class="col text-area-message" style="max-width: 82%;">
-                                <p class="mb-2 font-weight-bold <?= ($newMessages) ? 'text-warning' : ''; ?>"><?= fiomess($n) ?><?= ($newMessages) ? ' +' . $newMessages : ''; ?>
+                            <div class="col text-area-message">
+                                <p class="mb-2 font-weight-bold <?= ($companyMessages['newMessages']) ? 'text-warning' : ''; ?>"><?= $companyMessages['companyName'] ?><?= ($companyMessages['newMessages']) ? ' +' . $companyMessages['newMessages'] : ''; ?>
                                 </p>
-                                <span><?= ($lastMessage['sender'] == 1) ? 'Вы: ' : ''; ?> <?= $lastMessage['mes'] ?></span>
-                                <span class="date mr-2"><?= date('d.m H:i', $lastMessage['datetime']); ?></span>
                             </div>
                         </div>
-
                     </div>
                 </div>
             </a>
-        <?php } ?>
+            <?php foreach ($companyMessages['dialogs'] as $userId => $lastMessage): ?>
+            <div class="user-support d-none">
+                <a class="text-decoration-none text-dark" href="./<?= $userId ?>/">
+                    <div class="card mb-3 dialog-mail">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-2 pl-2">
+                                    <div class="user-pic position-relative" style="width:60px">
+                                        <img src="/<?= getAvatarLink($userId) ?>" class="avatar-img rounded-circle w-100"/>
+                                    </div>
+                                </div>
+                                <div class="col text-area-message" style="max-width: 82%;">
+                                    <p class="mb-2 font-weight-bold <?= ($lastMessage['newMessages']) ? 'text-warning' : ''; ?>"><?= ($lastMessage['message']['sender'] == 1) ? 'Вы' : fiomess($lastMessage['message']['sender']) ?><?= ($lastMessage['newMessages']) ? ' +' . $lastMessage['newMessages'] : ''; ?>
+                                    </p>
+                                    <span><?= ($lastMessage['message']['sender'] == $id) ? 'Вы: ' : ''; ?> <?= $lastMessage['message']['mes'] ?></span>
+                                    <span class="date mr-2"><?= date('d.m H:i', $lastMessage['message']['datetime']); ?></span>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <?php endforeach; ?>
+            </div>
+        <?php endforeach; ?>
     </div>
 </div>
 <script>
     $('#collapseTrigger').on('click', function () {
         $('#collapseUsers').collapse('toggle')
     });
+
+    $('.company-support-link').on('click', function (e) {
+        e.preventDefault();
+        var userElements = $(this).closest('.company-support').find('.user-support');
+        if (userElements.hasClass('d-none')){
+            userElements.removeClass('d-none');
+        } else {
+            userElements.addClass('d-none');
+
+        }
+    })
 </script>
