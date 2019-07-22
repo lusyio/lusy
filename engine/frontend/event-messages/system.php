f9d9ce3106fa3f53e1608d26496ecceef61b8320
Igor Garifyanov <garifianov.i@gmail.com>
2019-07-22 13:24:41 +0300

    Удалил закомментированный код

--- engine/frontend/event-messages/system.php
+++ engine/frontend/event-messages/system.php
@@ -62,17 +62,6 @@ if ($event['action'] == 'birthday') { // день рождения
 $month = ['', _("January"), _("February"), _("March"), _("April"), _("May"), _("June"), _("July"), _("August"), _("September"), _("October"), _("November"), _("December")];
 $monthNumber = date("n", $event['datetime']);
 ?>
-<!--<li data-event-id="--><? //= $event['event_id'] ?><!--"-->
-<!--    class="event system-event --><? //= ($event['view_status']) ? '' : 'new-event' ?><!--  readable-here">-->
-<!---->
-<!--    <span class="before --><? //= $bg ?><!-- --><? //= ($event['view_status']) ? '' : 'new-event' ?><!--"><i class="--><? //= $icon ?><!--"></i></span>-->
-<!--    <div class="position-relative">-->
-<!--        <span class="date">--><? //= date("d.m H:i", $event['datetime']); ?><!--</span>-->
-<!--        <img src="/--><? //= getAvatarLink($event['author_id']) ?><!--" class="avatar mr-2">-->
-<!--        <span class="font-weight-bold">--><? //= $event['name'] ?><!-- --><? //= $event['surname'] ?><!--</span>-->
-<!--    </div>-->
-<!--    <p class="eventText">--><? //= $action ?><!--</p>-->
-<!--</li>-->
 <a href="/../<?= $event['link'] ?>" class="text-decoration-none text-dark">
     <li data-event-id="<?= $event['event_id'] ?>"
         class="event <?= $event['action'] ?> <?= ($event['view_status']) ? '' : 'new-event' ?> <?= ($event['action'] == 'createtask') ? '' : 'readable-here' ?> system mb-3">
