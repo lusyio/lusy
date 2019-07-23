813250e938aaa3234bc9b0b6865e0462891497f4
Igor Garifyanov <garifianov.i@gmail.com>
2019-07-23 16:12:30 +0300

    Добавил имена менеджера и воркера в список задач для отчета

--- engine/ajax/report.php
+++ engine/ajax/report.php
@@ -42,12 +42,22 @@ function getPersonalStats($userId, $startDate, $endDate)
     $doneOutcomeQuery->execute();
     $doneOutcome = $doneOutcomeQuery->fetch(PDO::FETCH_COLUMN);
 
-    $tasksQuery = $pdo->prepare("SELECT DISTINCT t.id, t.name, t.description, t.status FROM tasks t LEFT JOIN events e ON t.id = e.task_id WHERE (t.manager = :userId OR t.worker = :userId) AND ((e.action IN ('workdone', 'canceltask') AND e.datetime >=:startDate AND t.datecreate < :endDate) OR (e.action IN ('new', 'inwork', 'overdue', 'postpone', 'pending', 'returned') AND t.datecreate < :endDate))");
+    $tasksQuery = $pdo->prepare("SELECT DISTINCT t.id, t.name, t.description, t.status, t.manager, u1.name AS managerName, u1.surname AS managerSurname, u1.email AS managerEmail, t.worker, u2.name AS workerName, u2.surname AS workerSurname, u2.email AS workerEmail FROM tasks t LEFT JOIN events e ON t.id = e.task_id LEFT JOIN users u1 ON t.manager = u1.id LEFT JOIN users u2 ON t.worker = u2.id  WHERE (t.manager = :userId OR t.worker = :userId) AND ((e.action IN ('workdone', 'canceltask') AND e.datetime >=:startDate AND t.datecreate < :endDate) OR (e.action IN ('new', 'inwork', 'overdue', 'postpone', 'pending', 'returned') AND t.datecreate < :endDate))");
     $tasksQuery->bindValue(':userId', (int) $userId, PDO::PARAM_INT);
     $tasksQuery->bindValue(':startDate', (int) $startDate, PDO::PARAM_INT);
     $tasksQuery->bindValue(':endDate', (int) $endDate, PDO::PARAM_INT);
     $tasksQuery->execute();
     $tasks = $tasksQuery->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($tasks as &$task) {
+        $task['managerFullName'] = trim($task['managerName'] . ' ' . $task['managerSurname']);
+        if ($task['managerFullName'] == '') {
+            $task['managerFullName'] = $task['managerEmail'];
+        }
+        $task['workerFullName'] = trim($task['workerName'] . ' ' . $task['workerSurname']);
+        if ($task['workerFullName'] == '') {
+            $task['workerFullName'] = $task['workerEmail'];
+        }
+    }
 
     $overdueQuery = $pdo->prepare("SELECT COUNT(*) FROM events e LEFT JOIN tasks t ON e.task_id = t.id WHERE e.action = 'overdue' AND t.worker = :userId AND e.datetime >= :startDate AND e.datetime < :endDate");
     $overdueQuery->bindValue(':userId', (int) $userId, PDO::PARAM_INT);
