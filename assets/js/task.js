36f6f1d7a3ea425e3ad2c2e4c55d1a06d294e7d5
Igor Garifyanov <garifianov.i@gmail.com>
2019-08-01 12:08:51 +0300

    К добавлении имени в чекбоксах

--- assets/js/task.js
+++ assets/js/task.js
@@ -710,7 +710,7 @@ $(document).ready(function () {
                 console.log(data);
                 if (data == 1){
                     $this.prop('checked', true);
-                    var userName = $('#fullUserName');
+                    var userName = $('#fullUserName').val();
                     $this.parents('.pure-material-checkbox').find('.small').text(userName);
                 } else{
                     $this.prop('checked', false);
