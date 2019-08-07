97f047da3f1ebeb3e009f7d4c1eec2af2fbb85be
Front <dimkaFPS@gmail.com>
2019-08-07 15:26:57 +0300

    все иконки в картинки

--- engine/phpmailer/templates/ru/task-overdue.php
+++ engine/phpmailer/templates/ru/task-overdue.php
@@ -9,7 +9,7 @@
                 <div align="center" class="block center" style="padding: 20px;text-align: center;max-width: 600px;background-color: #ffffff;">
                     <h1 style="font-weight: 900">Назначенная Вам задача просрочена</h1>
                     <p class="margin20t" style="margin-top: 20px;line-height: 2;">Прошел срок исполнения задачи - {$taskName}</p>
-                    <p class="margin20t" style="margin-top: 20px;line-height: 2;"><img style="opacity: 0.06;width: 150px;" src="/assets/svg/fire.svg"></p>
+                    <img width="200px" src="/upload/mail/fire.png" alt="" style="width: 200px">
                     <p class="margin30t" style="margin-top: 30px;line-height: 2;"><a class="button" href="https://s.lusy.io/task/{$taskId}/" style="background-color: #535ad3;border-color: #535ad3;color: #fff;font-weight: 400;text-align: center;vertical-align: middle;border: 1px solid transparent;padding-left: 25px;padding-right: 25px;font-size: 1rem;line-height: 1.5;border-radius: 20px;text-decoration: none;">Перейти к задаче</a></p>
                 </div>
             </div>
