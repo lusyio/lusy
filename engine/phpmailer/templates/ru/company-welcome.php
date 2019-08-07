6ad7ae5d0570a52a607355b276476f04fc867f6d
Front <dimkaFPS@gmail.com>
2019-08-07 11:31:07 +0300

    письма

--- engine/phpmailer/templates/ru/company-welcome.php
+++ engine/phpmailer/templates/ru/company-welcome.php
@@ -5,35 +5,23 @@
     <tbody>
     <tr>
         <td style="word-break:break-word;" align="center">
-            <div class="block center" style="vertical-align:top;text-align:left;width:100%;max-width:600px;background-color: #ffffff;">
-                <h1 style="font-weight: 900">Добро пожаловать в Lusy.io</h1>
-                <p class="margin20t" style="margin-top: 20px;line-height: 2;">Вы создали и подтвердили аккаунт компании {$companyName}.</p>
-                <p class="margin30t" style="text-align:center;margin-top: 30px;line-height: 2;"><a class="button" href="https://s.lusy.io/" style="background-color: #535ad3;border-color: #535ad3;color: #fff;font-weight: 400;text-align: center;vertical-align: middle;border: 1px solid transparent;padding-left: 25px;padding-right: 25px;font-size: 1rem;line-height: 1.5;border-radius: 20px;text-decoration: none;">Перейти в систему</a></p>
-            </div>
             <div class="main" style="vertical-align:top;text-align:left;width:100%;max-width:600px;background-color: #ffffff;">
-                <hr style="border-top: 1px solid #e4e4e4;border-bottom: none;">
-                <div class="margin20t" style="margin-top: 20px;">
-                    <table style="width: 100%;">
-                        <tr>
-                            <td width="40%" style="padding: 0px;"><img src="https://s.lusy.io/upload/email/ru/company-welcome-invite.jpg" style="width: 100%;max-width: 100%;"></td>
-                            <td width="60%" valign="top" style="padding: 0px;">
-                                <div class="padding20" style="padding: 20px;padding-top: 0px">
-                                    <h2 style="margin-top: 0px">Пригласите сотрудников!</h2>
-                                    <p class="margin20t" style="margin-top: 20px;line-height: 2;">Первым делом стоит пригласить сотрудников в систему. Для этого зайдите в <a href="https://s.lusy.io/invite/">раздел приглашений</a>.</p>
-                                </div>
-                            </td>
-                        </tr>
-                    </table>
+                <div class="block center" style="vertical-align:top;text-align:left;width:100%;max-width:600px;background-color: #ffffff;">
+                    <h1 style="font-weight: 900">Добро пожаловать в Lusy.io</h1>
+                    <p class="margin20t" style="margin-top: 20px;line-height: 2;">Вы создали и подтвердили аккаунт компании {$companyName}.</p>
+                    <p class="margin30t" style="text-align:center;margin-top: 30px;line-height: 2;"><a class="button" href="https://s.lusy.io/" style="background-color: #535ad3;border-color: #535ad3;color: #fff;font-weight: 400;text-align: center;vertical-align: middle;border: 1px solid transparent;padding-left: 25px;padding-right: 25px;font-size: 1rem;line-height: 1.5;border-radius: 20px;text-decoration: none;">Перейти в систему</a></p>
                 </div>
                 <hr style="border-top: 1px solid #e4e4e4;border-bottom: none;">
-                <div class="margin20t" style="margin-top: 20px;">
-                    <table style="width: 100%;">
+                <div class="margin20t" style="margin-top: 20px;background-color: #ffffff">
+                    <table style="width: 100%;background-color: #ffffff">
                         <tr>
-                            <td width="40%" style="padding: 0px;"><img src="https://s.lusy.io/upload/email/ru/user-welcome-noty.jpg" style="width: 100%;max-width: 100%;"></td>
-                            <td width="60%" valign="top" style="padding: 0px;">
+                            <td style="padding: 0px;"><img src="/upload/newtask.jpg" style="width: 100%;max-width: 100%;"></td>
+                        </tr>
+                        <tr>
+                            <td align="center" valign="top" style="padding: 0px;">
                                 <div class="padding20" style="padding: 20px;padding-top: 0px">
-                                    <h2 style="margin-top: 0px">Будь в курсе событий!</h2>
-                                    <p class="margin20t" style="margin-top: 20px;line-height: 2;">В верхнем меню есть блок уведомлений, который подскажет, есть ли у вас изменения в задачах, добавлены ли в них новые комментарии и т.д.</p>
+                                    <h2 style="margin-top: 0px">Создавайте задачи!</h2>
+                                    <p class="margin20t" style="margin-top: 20px;line-height: 2;">Максимально гибко настройте задачу. Для этого зайдите в <a href="https://s.lusy.io/task/new/">раздел создания задачи</a>.</p>
                                 </div>
                             </td>
                         </tr>
