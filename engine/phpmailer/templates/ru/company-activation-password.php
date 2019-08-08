935a9e5264df1081279090fbeb9405b038941e69
Front <dimkaFPS@gmail.com>
2019-08-08 16:48:54 +0300

    новое письмо активашион пассворд

--- engine/phpmailer/templates/ru/company-activation-password.php
+++ engine/phpmailer/templates/ru/company-activation-password.php
@@ -1,9 +1,23 @@
-<div class="block center" style="padding: 20px;text-align: center;">
-    <h1>Создан аккаунт в системе Lusy.io</h1>
-    <p style="line-height: 2;">Используйте следующие данные для входа в систему</p>
-    <p style="line-height: 2;">логин/e-mail: {$email}</p>
-    <p style="line-height: 2;">пароль: {$password}</p>
-    <p style="line-height: 2;">Для подтверждения электронной почты нажмите на кнопку:</p>
-    <div class="margin30t" style="margin-top: 30px;"><a class="button" href="{$activationLink}" style="background-color: #5a93df;border-color: #5a93df;color: #fff;font-weight: 400;text-align: center;vertical-align: middle;border: 1px solid transparent;padding: .375rem .75rem;font-size: 1rem;line-height: 1.5;border-radius: .25rem;text-decoration: none;">Подтвердить e-mail</a></div>
-    <div class="margin30t" style="margin-top: 30px;">Если ссылка не активна или не работает, скопируйте этот адрес в адресную строку:<br>{$activationLink}</div>
+<div style="display: none;font-size: 1px;color: #333333;line-height: 1px;max-height: 0px;opacity: 0;overflow: hidden">
+    Поздравляем! Вы успешно создали аккаунт в Lusy.io. Логин и пароль находятся в этом письме.
 </div>
+<table cellpadding="0" cellspacing="0" width="100%" border="0">
+    <tbody>
+    <tr>
+        <td style="word-break:break-word;" align="center">
+            <div style="vertical-align:top;text-align:left;max-width:600px;">
+                <div align="center" class="block center" style="padding: 20px;text-align: center;max-width: 600px;background-color: #ffffff;">
+                    <h1 style="font-size: 30px;font-weight: 900; color:#000000;">Создан аккаунт в системе Lusy.io</h1>
+                    <p class="margin20t" style="margin-top: 20px;line-height: 2;font-size: 16px;color: #353b41;">Используйте следующие данные для входа в систему</p>
+                    <p class="margin20t" style="margin-top: 20px;line-height: 2;font-size: 16px;color: #353b41;">логин/e-mail: {$email}</p>
+                    <p class="margin20t" style="margin-top: 20px;line-height: 2;font-size: 16px;color: #353b41;">пароль: {$password}</p>
+                    <img width="200px" src="https://lusy.io/public/images/mail/at.png" alt="" style="width: 200px">
+                    <p class="margin20t" style="margin-top: 20px;line-height: 2;font-size: 16px;color: #353b41;">Для подтверждения электронной почты нажмите на кнопку:</p>
+                    <p class="margin30t" style="margin-top: 30px;line-height: 2;"><a class="button" href="{$activationLink}" style="padding: .375rem .75rem;background-color: #535ad3;border-color: #535ad3;color: #fff;font-weight: 400;text-align: center;vertical-align: middle;border: 1px solid transparent;padding-left: 25px;padding-right: 25px;font-size: 1rem;line-height: 1.5;border-radius: 20px;text-decoration: none;">Подтвердить e-mail</a></p>
+                    <div class="margin30t" style="margin-top: 30px;line-height: 2;font-size: 16px;color: #353b41;">Если ссылка не активна или не работает, скопируйте этот адрес в адресную строку:<br>{$activationLink}</div>
+                </div>
+            </div>
+        </td>
+    </tr>
+    </tbody>
+</table>
