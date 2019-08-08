8d564cbc9e324eda0d55141e27cfc8294dacceea
Front <dimkaFPS@gmail.com>
2019-08-08 12:58:07 +0300

    сообщения

--- engine/phpmailer/templates/ru/comment.php
+++ engine/phpmailer/templates/ru/comment.php
@@ -10,7 +10,7 @@
                     <h1 style="font-size: 30px;font-weight: 900; color:#000000;">Новый комментарий</h1>
                     <p class="margin20t" style="margin-top: 20px;line-height: 2;font-size: 16px;color: #353b41;">{$authorName} оставил комментарий к задаче - {$taskName}</p>
                     <img width="200px" src="https://lusy.io/public/images/mail/feedback.png" alt="" style="width: 200px">
-                    <p class="margin20t" style="margin-top: 20px;line-height: 2;font-size: 16px;color: #353b41;">{$comment}</p>
+                    <p class="margin20t" style="margin-top: 20px;line-height: 2;font-size: 16px;color: #353b41;">{$commentText}</p>
                     <p class="margin30t" style="margin-top: 30px;line-height: 2;"><a class="button" href="https://s.lusy.io/task/{$taskId}/#{$commentId}" style="padding: .375rem .75rem;background-color: #535ad3;border-color: #535ad3;color: #fff;font-weight: 400;text-align: center;vertical-align: middle;border: 1px solid transparent;padding-left: 25px;padding-right: 25px;font-size: 1rem;line-height: 1.5;border-radius: 20px;text-decoration: none;">Перейти к комментарию</a></p>
                 </div>
             </div>
--- engine/phpmailer/templates/ru/message.php
+++ engine/phpmailer/templates/ru/message.php
@@ -10,6 +10,7 @@
                     <h1 style="font-size: 30px;font-weight: 900; color:#000000;">Новое личное сообщение</h1>
                     <p class="margin20t" style="margin-top: 20px;line-height: 2;font-size: 16px;color: #353b41;">{$authorName} Отправил вам личное сообщение</p>
                     <img width="200px" src="https://lusy.io/public/images/mail/send.png" alt="" style="width: 200px">
+                    <p class="margin20t" style="margin-top: 20px;line-height: 2;font-size: 16px;color: #353b41;">{$messageText}</p>
                     <p class="margin30t" style="margin-top: 30px;line-height: 2;"><a class="button" href="https://s.lusy.io/mail/" style="padding: .375rem .75rem;background-color: #535ad3;border-color: #535ad3;color: #fff;font-weight: 400;text-align: center;vertical-align: middle;border: 1px solid transparent;padding-left: 25px;padding-right: 25px;font-size: 1rem;line-height: 1.5;border-radius: 20px;text-decoration: none;">Перейти в личные сообщения</a></p>
                 </div>
             </div>
