c0deb5e9eaf96401026dd064027c022bf4821443
Igor Garifyanov <garifianov.i@gmail.com>
2019-08-15 12:31:19 +0300

    Изменил версию createtask.js на 3

--- engine/frontend/other/tasknew.php
+++ engine/frontend/other/tasknew.php
@@ -440,7 +440,7 @@ $borderColor = [
         </div>
     </div>
 </div>
-<script src="/assets/js/createtask.js?n=2"></script>
+<script src="/assets/js/createtask.js?n=3"></script>
 <?php if (($tariff == 1 || $tryPremiumLimits['cloud'] < 3) || ($taskEdit && $hasCloudUploads)): ?>
     <script type="text/javascript">
         //=======================Google Drive==========================
