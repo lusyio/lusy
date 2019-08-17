cfd5e53965deb51a564eb87dd1ce2e166d72d4bd
Kelevra <kelevra@MacBook-Pro-Dmitrij-2.local>
2019-08-17 10:21:44 +0300

    Изменил версию

--- engine/frontend/other/footer.php
+++ engine/frontend/other/footer.php
@@ -1,7 +1,7 @@
 <div class="container footer">
     <div class="row">
         <div class="col-sm-12">
-            <a href="https://lusy.io/ru/history/" class="float-left small text-secondary" target="_blank">LUSY.IO v1.0.1</a>
+            <a href="https://lusy.io/ru/history/" class="float-left small text-secondary" target="_blank">LUSY.IO v1.0.2</a>
             <?php if (!is_null($id)): ?>
             <a href="/mail/1/" class="small text-secondary float-right">Обратная связь</a>
             <?php endif; ?>
