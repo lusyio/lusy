67860684ff6a808134135fd5846bfb2fb7ec6280
Igor Garifyanov <garifianov.i@gmail.com>
2019-08-22 09:52:05 +0300

    Revert "Тестирование progressive web app"
    This reverts commit 0793390bcf5594ac2c1f8d5159f67d0bb51d9d17.

--- sw.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-importScripts('/sw-toolbox.js');
-toolbox.precache(['/dashboard/', 'assets/css/custom.css']);
-toolbox.router.get('/upload/avatar/*', toolbox.cacheFirst);
-toolbox.router.get('/*', toolbox.networkFirst, {networkTimeoutSeconds: 5});
