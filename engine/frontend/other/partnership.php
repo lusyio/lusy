802b2205a77610416bb47e38bb837a81a8857780
mastekator <akmiskam@yandex.ru>
2019-09-23 15:15:16 +0300

    страница partnership

--- engine/frontend/other/partnership.php
+++ engine/frontend/other/partnership.php
@@ -1,1 +1,167 @@
-<?php
+<div class="card partner-card">
+    <div class="card-body">
+        <h5>
+            Ваша реферальная ссылка:
+        </h5>
+        <div class="partner-link">
+            <span>
+                <i data-clipboard-text="https://lusy.io/reflink"
+                   class="fas fa-link copy-partner-link" data-toggle="tooltip" data-placement="bottom" title=""
+                   data-original-title="Скопировать ссылку">
+                </i>
+            </span>
+            <a class="text-decoration-none" href="#">
+                https://lusy.io/reflink
+            </a>
+        </div>
+    </div>
+</div>
+
+<div class="card partner-card mt-4">
+    <div class="card-body">
+        <h5>
+            Ваш промокод для друзей:
+        </h5>
+        <div class="partner-link">
+            <span>
+                <i data-clipboard-text="refpromocode"
+                   class="far fa-copy copy-partner-link" data-toggle="tooltip" data-placement="bottom" title=""
+                   data-original-title="Скопировать промокод">
+                </i>
+            </span>
+            <span>
+               refpromocode
+            </span>
+        </div>
+        <div class="row mt-5">
+            <div class="col text-center">
+                <span class="partner-icon">
+                    <i class="fas fa-ad"></i>
+                </span>
+                <a class="text-decoration-none" href="https://lusy.io/ru/partners/#waystab">Способы продвижения</a>
+            </div>
+            <div class="col text-center">
+                <span class="partner-icon">
+                    <i class="fas fa-bullhorn"></i>
+                </span>
+                <a class="text-decoration-none" href="https://lusy.io/ru/partners/#marketingtab">Рекламные материалы</a>
+            </div>
+            <div class="col text-center">
+                <span class="partner-icon">
+                    <i class="fas fa-book-reader"></i>
+                </span>
+                <a class="text-decoration-none" href="https://lusy.io/ru/partners/#rulestab">Правила программы</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="card partner-card mt-4">
+    <div class="card-body">
+        <h5>
+            Статистика:
+        </h5>
+        <div class="row mt-5">
+            <div class="col text-center">
+                <p class="text-success partner-stat-number">
+                    30
+                </p>
+                <p class="partner-stat-name">
+                    Всего зарегистрировано
+                </p>
+            </div>
+            <div class="col text-center">
+                <p class="partner-stat-number">
+                    <span class="badge badge-warning">15</span>
+                    <span class="badge badge-success">30</span>
+                </p>
+                <p class="partner-stat-name">
+                    Ожидает подтверждение/Одобренные
+                </p>
+            </div>
+            <div class="col text-center">
+                <p class="partner-stat-bonus partner-stat-number">
+                    30 дней
+                </p>
+                <p class="partner-stat-name">
+                    Ожидаемый бонус
+                </p>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col">
+                <h5 class="text-center">
+                    Получи Premium навсегда!
+                </h5>
+                <div class="card partner-progress mt-4">
+                    <div class="card-body">
+                        <div class="row text-center text-white position-relative">
+                            <hr class="partner-hr">
+                            <div class="col">
+                                <i class="fas fa-dot-circle dot-partner"></i>
+                                <p class="mb-0 mt-2 small">5 компаний</p>
+                            </div>
+                            <div class="col">
+                                <i class="far fa-dot-circle dot-partner"></i>
+                                <p class="mb-0 mt-2 small">10 компаний</p>
+                            </div>
+                            <div class="col">
+                                <i class="far fa-dot-circle dot-partner"></i>
+                                <p class="mb-0 mt-2 small">20 компаний</p>
+                            </div>
+                            <div class="col">
+                                <i class="far fa-dot-circle dot-partner"></i>
+                                <p class="mb-0 mt-2 small">30 компаний</p>
+                            </div>
+                            <div class="col">
+                                <i class="fas fa-crown dot-partner"></i>
+                                <p class="mb-0 mt-2 small">40 компаний</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="card partner-card mt-4">
+    <div class="card-body">
+        <div class="row">
+            <div class="col-5">
+                <h5 class="mt-5 pt-5">
+                    Подзравляем! Вам доступна бесконечная подписка
+                </h5>
+            </div>
+            <div class="col-7">
+                <img src="/upload/winner-partner.jpg" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+
+<h5 class="mt-3 mb-3">
+    Привлеченные компании:
+</h5>
+<div class="card mb-1 payment-card partner-card pt-1 pb-1">
+    <div class="card-body row">
+        <div class=" col-1">
+            <i class="fas fa-check text-success paymentIcon"></i>
+        </div>
+        <div class="col">
+            <div class="row m-0">
+                Рога и копыта
+            </div>
+            <div class="row m-0 text-secondary">
+                31.07.2019 18:58
+            </div>
+        </div>
+        <div class="text-success col-2 text-center">
+            Успешно
+        </div>
+    </div>
+</div>
+<script src="/assets/js/clipboard.min.js"></script>
+<script>
+    new ClipboardJS('.copy-partner-link');
+</script>
