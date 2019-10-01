f7a1db09779f5b4a9c623e3a37761157b704a625
Kelevra <kelevra@MacBook-Pro-Dmitrij-2.local>
2019-10-01 12:41:39 +0300

    hotjar

--- engine/frontend/other/header.php
+++ engine/frontend/other/header.php
@@ -16,6 +16,17 @@ if (empty($title)) {
     <script type="text/javascript" src="/assets/js/jquery.min.js"></script>
     <script type="text/javascript" src="/assets/js/popper.min.js"></script>
     <title><?= $title ?></title>
+    <!-- Hotjar Tracking Code for s.lusy.io -->
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:1508523,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
 </head>
 <body class="anim-show">
 <?php if (!empty($_SESSION['auth'])) { ?>
