ace0c836680503ea980c46108fcacb5534046bc4
Kelevra <kelevra@MacBook-Pro-Dmitrij-2.local>
2019-10-01 12:40:49 +0300

    test

---
+++ engine/frontend/other/kanban.php
@@ -0,0 +1,45 @@
+<style>
+    .block-kanban {
+        min-width: 272px;
+        margin-right: 10px;
+    }
+
+    .bg-kanban {
+        background-color: #f3f3f3;
+    }
+</style>
+<div style="overflow-x: scroll;min-height: 600px">
+
+    <div class="d-flex">
+
+        <?php for ($i = 1; $i <= 10; $i++) : ?>
+
+            <div class="block-kanban">
+                <div class="bg-kanban p-2" style="border-radius: 5px">
+                    <div class="card-body">
+                        <strong>Статус задач <?= $i; ?></strong>
+                    </div>
+
+                    <?php $p = 0; ?>
+                    <?php foreach ($new as $n) : ?>
+                    <a href="/tasks/<?= $n['id']; ?>/" target="_blank">
+                        <div class="card mb-3">
+                            <div class="card-body">
+                                <strong><?= $n['name']; ?></strong>
+                            </div>
+                        </div>
+                    </a>
+                        <?php $p++;
+                        if ($p > $i) {
+                            break;
+                        }
+                        ?>
+
+                    <?php endforeach; ?>
+                </div>
+            </div>
+
+        <?php endfor; ?>
+    </div>
+
+</div>
