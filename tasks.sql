23f580c222b84bd39f80427fa9591ced47899a77
Igor Garifyanov <garifianov.i@gmail.com>
2019-10-11 14:32:43 +0300

    БД!!! Правка скриптов бд

--- tasks.sql
+++ tasks.sql
@@ -1,5 +1,3 @@
 alter table tasks add repeat_task int null;
 
-alter table tasks add repeat_type int default 0 null;
-
-alter table tasks alter column repeat_type set default null;
+alter table tasks add repeat_type int null;
