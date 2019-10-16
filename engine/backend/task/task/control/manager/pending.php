f8e0199f3791fc77a4e8ef99961a8fe37c1ab490
Igor Garifyanov <garifianov.i@gmail.com>
2019-10-16 17:28:19 +0300

    Исправил вывод списка файлов в отчете задачи

--- engine/backend/task/task/control/manager/pending.php
+++ engine/backend/task/task/control/manager/pending.php
@@ -4,6 +4,6 @@ $report = DBOnce('comment', 'comments', "idtask=" . $idtask . " and status = 're
 $commentId = DBOnce('id', 'comments', "idtask=" . $idtask . " and status = 'report' order by `datetime` desc");
 $report = nl2br($report);
 $filesQuery = $pdo->prepare('SELECT file_id, file_name, file_size, file_path, comment_id, cloud FROM uploads WHERE comment_id = :commentId and comment_type = :commentType');
-$filesQuery->execute(array(':commentId' => $commentId, ':commentType' => 'comment'));
+$filesQuery->execute(array(':commentId' => $commentId, ':commentType' => 'task'));
 $files = $filesQuery->fetchAll(PDO::FETCH_ASSOC);
 ?>
