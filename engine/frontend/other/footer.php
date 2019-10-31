984507a4f823fd30fee8e58034763a1d8160a819
Kelevra <kelevra@MacBook-Pro-Dmitrij-2.local>
2019-10-31 09:46:01 +0300

    версия

--- engine/frontend/other/footer.php
+++ engine/frontend/other/footer.php
@@ -2,7 +2,7 @@
     <div class="row">
         <div class="col-sm-12">
             <a href="https://lusy.io/ru/history/" class="float-left small text-secondary" target="_blank">LUSY.IO
-                v1.0.4</a>
+                v1.0.5</a>
             <?php if (!is_null($id)): ?>
                 <a href="/mail/1/" class="small text-secondary float-right">Обратная связь</a>
             <?php endif; ?>
