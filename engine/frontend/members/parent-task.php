b00a09e0a7268bc164500a2bec00c80a063fa47b
Igor Garifyanov <garifianov.i@gmail.com>
2020-06-26 16:25:41 +0300

    Удалил текущую задачу из списка надзадач

--- engine/frontend/members/parent-task.php
+++ engine/frontend/members/parent-task.php
@@ -4,7 +4,8 @@
             Список пуст
         </div>
         <?php foreach ($parentTasks as $parentTaskItem): ?>
-        <div val="<?php echo $parentTaskItem['id']; ?>" class="select-subtask <?= ($taskEdit && $parentTaskItem['id'] == $parentTask) ? 'd-none' : '' ?>">
+            <?php if ($taskEdit && $parentTaskItem['id'] == $taskId) continue; ?>
+            <div val="<?php echo $parentTaskItem['id']; ?>" class="select-subtask <?= ($taskEdit && $parentTaskItem['id'] == $parentTask) ? 'd-none' : '' ?>">
             <div class="row">
                 <div class="col pr-0 text-left text-area-message ml-2 border-left-tasks <?= $borderColor[$parentTaskItem['status']] ?>">
                     <span class="mb-1 add-coworker-text"><?= $parentTaskItem['name']; ?></span>
