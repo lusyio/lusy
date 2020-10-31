05d4606306e8e8095b3acdca17ab6127afeb2953
lusyio <mr-kelevras@yandex.ru>
2020-10-31 21:14:45 +0300

    Update tasknew.php

--- engine/backend/other/tasknew.php
+++ engine/backend/other/tasknew.php
@@ -27,7 +27,7 @@ if ($isCeo) {
 } else {
     $parentTasksQuery = $pdo->prepare("SELECT id, name, description, status, manager, worker, idcompany, report, view, view_status, author, datecreate, datepostpone, datedone 
 FROM tasks 
-WHERE status NOT IN ('done', 'canceled', 'planned') AND parent_task IS NULL AND repeat_type < 1 AND idcompany = :companyId");
+WHERE status NOT IN ('done', 'canceled', 'planned') AND parent_task IS NULL AND repeat_type < 1 AND idcompany = ".$idc);
     $parentTasksQuery->execute([':managerId' => $id]);
     $parentTasks = $parentTasksQuery->fetchAll(PDO::FETCH_ASSOC);
 }
